fd9275313b348f3a5bfa6a13583c141625db1b97
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 01:33:28 +0300

    Add Anton Chekhov profile

---
+++ src/data/writers/russia/chekhov.ts
@@ -0,0 +1,2 @@
+import type { WriterProfile } from "../types";
+export const chekhov: WriterProfile = {id:"anton-chekhov",fullName:"Антон Павлович Чехов",birth:"29 января 1860",death:"15 июля 1904",country:"Россия",movement:"Реализм",biography:"Русский писатель, драматург и врач, классик мировой литературы.",works:["Вишнёвый сад","Три сестры","Чайка","Палата №6"],awards:[],places:["Мелихово","Ялта"]};
