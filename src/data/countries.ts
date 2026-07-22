3394db4353e6dda2f397278550aed71e867cefb8
Kosyat128 <Kosyat128@gmail.com>
2026-07-22 20:03:52 +0300

    Update countries.ts

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,5 +1,110 @@
 import type { Country } from "./types";
-import { france } from "./writers/france";
-import { russia } from "./writers/russia";
 
-export const countries: Country[] = [france, russia];
+import { uk } from "./writers/uk";
+import { france } from "./writers/france";
+import { germany } from "./writers/germany";
+import { italy } from "./writers/italy";
+import { portugal } from "./writers/portugal";
+import { netherlands } from "./writers/netherlands";
+import { switzerland } from "./writers/switzerland";
+import { austria } from "./writers/austria";
+import { belgium } from "./writers/belgium";
+import { spain } from "./writers/spain";
+
+import { ireland } from "./writers/ireland";
+import { denmark } from "./writers/denmark";
+import { sweden } from "./writers/sweden";
+import { norway } from "./writers/norway";
+import { finland } from "./writers/finland";
+
+import { poland } from "./writers/poland";
+import { czechia } from "./writers/czechia";
+import { hungary } from "./writers/hungary";
+import { romania } from "./writers/romania";
+import { bulgaria } from "./writers/bulgaria";
+
+import { russia } from "./writers/russia";
+import { ukraine } from "./writers/ukraine";
+import { belarus } from "./writers/belarus";
+import { turkey } from "./writers/turkey";
+import { greece } from "./writers/greece";
+
+import { china } from "./writers/china";
+import { japan } from "./writers/japan";
+import { india } from "./writers/india";
+
+import { usa } from "./writers/usa";
+import { canada } from "./writers/canada";
+import { mexico } from "./writers/mexico";
+import { argentina } from "./writers/argentina";
+import { brazil } from "./writers/brazil";
+import { chile } from "./writers/chile";
+
+import { southAfrica } from "./writers/south_africa";
+import { egypt } from "./writers/egypt";
+import { ghana } from "./writers/ghana";
+import { kenya } from "./writers/kenya";
+import { nigeria } from "./writers/nigeria";
+import { morocco } from "./writers/morocco";
+import { algeria } from "./writers/algeria";
+import { tanzania } from "./writers/tanzania";
+import { zimbabwe } from "./writers/zimbabwe";
+
+import { australia } from "./writers/australia";
+import { newZealand } from "./writers/new_zealand";
+
+
+export const countries: Country[] = [
+  uk,
+  france,
+  germany,
+  italy,
+  portugal,
+  netherlands,
+  switzerland,
+  austria,
+  belgium,
+  spain,
+
+  ireland,
+  denmark,
+  sweden,
+  norway,
+  finland,
+
+  poland,
+  czechia,
+  hungary,
+  romania,
+  bulgaria,
+
+  russia,
+  ukraine,
+  belarus,
+  turkey,
+  greece,
+
+  china,
+  japan,
+  india,
+
+  usa,
+  canada,
+  mexico,
+  argentina,
+  brazil,
+  chile,
+
+  southAfrica,
+  egypt,
+  ghana,
+  kenya,
+  nigeria,
+  morocco,
+  algeria,
+  tanzania,
+  zimbabwe,
+
+  australia,
+  newZealand
+];
