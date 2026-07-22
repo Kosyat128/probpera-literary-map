d35fbdbcbcb7e8fa6bc281b0d2a4e335d1a8a8ba
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 01:52:27 +0300

    Create Russian writers registry for LiteraryMap

---
+++ src/data/writers/russia/index.ts
@@ -0,0 +1,51 @@
+import { tolstoy } from "./tolstoy";
+import { pushkin } from "./pushkin";
+import { dostoevsky } from "./dostoevsky";
+import { chekhov } from "./chekhov";
+import { turgenev } from "./turgenev";
+import { bulgakov } from "./bulgakov";
+import { nabokov } from "./nabokov";
+import { gogol } from "./gogol";
+import { lermontov } from "./lermontov";
+import { leskov } from "./leskov";
+import { kuprin } from "./kuprin";
+import { bunin } from "./bunin";
+import { goncharov } from "./goncharov";
+import { pasternak } from "./pasternak";
+import { akhmatova } from "./akhmatova";
+import { tsvetaeva } from "./tsvetaeva";
+import { yesenin } from "./yesenin";
+import { solzhenitsyn } from "./solzhenitsyn";
+import { brodsky } from "./brodsky";
+import { mayakovsky } from "./mayakovsky";
+import { mandelshtam } from "./mandelshtam";
+import { sholokhov } from "./sholokhov";
+import { gorky } from "./gorky";
+import { blok } from "./blok";
+
+export const russianWriters = [
+  tolstoy,
+  pushkin,
+  dostoevsky,
+  chekhov,
+  turgenev,
+  bulgakov,
+  nabokov,
+  gogol,
+  lermontov,
+  leskov,
+  kuprin,
+  bunin,
+  goncharov,
+  pasternak,
+  akhmatova,
+  tsvetaeva,
+  yesenin,
+  solzhenitsyn,
+  brodsky,
+  mayakovsky,
+  mandelshtam,
+  sholokhov,
+  gorky,
+  blok
+];
