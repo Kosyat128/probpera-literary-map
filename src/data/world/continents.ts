7caf34a85bf048fab0033d9fcbd20b34548d12da
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 02:46:04 +0300

    Add continent structure for world expansion

---
+++ src/data/world/continents.ts
@@ -0,0 +1,7 @@
+export const continents={
+ europe:['russia','france','uk','germany','italy','spain'],
+ asia:['japan','china','india','turkey'],
+ america:['usa','brazil','argentina','mexico'],
+ africa:['egypt','south-africa','nigeria'],
+ oceania:['australia','new-zealand']
+};
