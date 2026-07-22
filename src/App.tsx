f6421f3ef9ad20a17962592ba08656d835b2fd26
Kosyat128 <Kosyat128@gmail.com>
2026-07-22 20:16:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,131 @@ import WriterPanel from "./components/WriterPanel";
 
 import { countries } from "./data/countries";
 
+import type { Country } from "./data/types";
+
+
 export default function App() {
-  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+
+
+  const [selectedCountry, setSelectedCountry] =
+    useState<Country | null>(
+      countries[0] ?? null
+    );
+
+
+  if (!selectedCountry) {
+
+    return (
+
+      <div>
+
+        База стран не загружена
+
+      </div>
+
+    );
+
+  }
+
+
+
+  const handleCountrySelect = (
+    countryName: string
+  ) => {
+
+
+    const country = countries.find(
+      (item) =>
+        item.name === countryName
+    );
+
+
+    if (country) {
+
+      setSelectedCountry(country);
+
+    }
+
+  };
+
+
 
   return (
+
     <>
+
       <Header title="LiteraryMap" />
 
+
       <div
+
         style={{
+
           display: "flex",
+
           height: "calc(100vh - 80px)",
+
         }}
+
       >
+
+
         <Sidebar
-          items={countries.map((country) => country.name)}
-          selectedItem={selectedCountry.name}
-          onSelect={(countryName) => {
-            const country = countries.find((c) => c.name === countryName);
-            if (country) {
-              setSelectedCountry(country);
-            }
-          }}
+
+
+          items={
+            countries.map(
+              (country) =>
+                country.name
+            )
+          }
+
+
+          selectedItem={
+            selectedCountry.name
+          }
+
+
+          onSelect={
+            handleCountrySelect
+          }
+
+
         />
+
+
 
         <WorldMap
-          selectedCountry={selectedCountry.name}
-          onCountrySelect={(countryName) => {
-            const country = countries.find((c) => c.name === countryName);
-            if (country) {
-              setSelectedCountry(country);
-            }
-          }}
+
+
+          selectedCountry={
+            selectedCountry.name
+          }
+
+
+          onCountrySelect={
+            handleCountrySelect
+          }
+
+
         />
 
-        <WriterPanel country={selectedCountry} />
+
+
+        <WriterPanel
+
+          country={
+            selectedCountry
+          }
+
+        />
+
+
       </div>
+
+
     </>
+
   );
+
 }
