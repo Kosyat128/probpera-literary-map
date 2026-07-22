5c224748127dd49874485d98a70cb1effe46dd88
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 01:38:02 +0300

    Add Ivan Bunin profile

---
+++ src/data/writers/russia/bunin.ts
@@ -0,0 +1,2 @@
+import type { WriterProfile } from "../types";
+export const bunin: WriterProfile={id:"ivan-bunin",fullName:"Иван Алексеевич Бунин",birth:"22 октября 1870",death:"8 ноября 1953",country:"Россия",movement:"Реализм",biography:"Русский писатель и поэт, первый русский лауреат Нобелевской премии по литературе.",works:["Жизнь Арсеньева","Тёмные аллеи","Господин из Сан-Франциско"],awards:["Нобелевская премия по литературе (1933)"],places:["Воронеж","Париж"]};
