5c84ac41a801f4092528885d1ff3f09a17e48c3c
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 02:20:26 +0300

    Add additional 20 Russian writer profiles for LiteraryMap

---
+++ src/data/writers/russia/additional.ts
@@ -0,0 +1,24 @@
+import type { WriterProfile } from "../types";
+
+export const additionalRussianWriters: WriterProfile[] = [
+ {id:"ivan-goncharov",fullName:"Иван Александрович Гончаров",birth:"18 июня 1812",country:"Россия",movement:"Реализм",biography:"Русский писатель XIX века.",works:["Обломов"],awards:[],places:["Симбирск","Санкт-Петербург"]},
+ {id:"maxim-gorky",fullName:"Максим Горький",birth:"28 марта 1868",country:"Россия",movement:"Реализм",biography:"Русский писатель и драматург.",works:["На дне","Мать"],awards:[],places:["Нижний Новгород"]},
+ {id:"andrey-platonov",fullName:"Андрей Платонович Платонов",birth:"1 сентября 1899",country:"Россия",movement:"Модернизм",biography:"Русский писатель XX века.",works:["Котлован","Чевенгур"],awards:[],places:["Воронеж"]},
+ {id:"evgeny-zamyatin",fullName:"Евгений Иванович Замятин",birth:"1 февраля 1884",country:"Россия",movement:"Модернизм",biography:"Русский писатель, автор антиутопии.",works:["Мы"],awards:[],places:["Лебедянь","Париж"]},
+ {id:"alexander-kuprin",fullName:"Александр Иванович Куприн",birth:"7 сентября 1870",country:"Россия",movement:"Реализм",biography:"Русский писатель.",works:["Олеся","Гранатовый браслет"],awards:[],places:["Гатчина"]},
+ {id:"valentin-rasputin",fullName:"Валентин Григорьевич Распутин",birth:"15 марта 1937",country:"Россия",movement:"Деревенская проза",biography:"Русский писатель XX века.",works:["Прощание с Матёрой"],awards:[],places:["Иркутск"]},
+ {id:"viktor-astafyev",fullName:"Виктор Петрович Астафьев",birth:"1 мая 1924",country:"Россия",movement:"Реализм",biography:"Русский писатель.",works:["Царь-рыба"],awards:[],places:["Красноярск"]},
+ {id:"vasily-shukshin",fullName:"Василий Макарович Шукшин",birth:"25 июля 1929",country:"Россия",movement:"Реализм",biography:"Русский писатель, режиссёр и актёр.",works:["Калина красная"],awards:[],places:["Алтай"]},
+ {id:"alexander-fadeev",fullName:"Александр Александрович Фадеев",birth:"24 декабря 1901",country:"Россия",movement:"Социалистический реализм",biography:"Русский писатель.",works:["Молодая гвардия"],awards:[],places:["Владивосток"]},
+ {id:"ilya-ilf",fullName:"Илья Ильф",birth:"3 октября 1897",country:"Россия",movement:"Сатира",biography:"Русский писатель-сатирик.",works:["Двенадцать стульев"],awards:[],places:["Одесса"]},
+ {id:"yevgeny-petrov",fullName:"Евгений Петров",birth:"13 декабря 1902",country:"Россия",movement:"Сатира",biography:"Русский писатель.",works:["Золотой телёнок"],awards:[],places:["Одесса"]},
+ {id:"alexey-tolstoy",fullName:"Алексей Николаевич Толстой",birth:"10 января 1883",country:"Россия",movement:"Реализм",biography:"Русский писатель.",works:["Пётр Первый","Аэлита"],awards:[],places:["Самара"]},
+ {id:"konstantin-paustovsky",fullName:"Константин Георгиевич Паустовский",birth:"31 мая 1892",country:"Россия",movement:"Реализм",biography:"Русский писатель.",works:["Повесть о жизни"],awards:[],places:["Москва"]},
+ {id:"mikhail-zoshchenko",fullName:"Михаил Михайлович Зощенко",birth:"10 августа 1894",country:"Россия",movement:"Сатира",biography:"Русский писатель.",works:["Рассказы Назара Ильича господина Синебрюхова"],awards:[],places:["Санкт-Петербург"]},
+ {id:"daniil-kharms",fullName:"Даниил Иванович Хармс",birth:"30 декабря 1905",country:"Россия",movement:"Авангард",biography:"Русский писатель и поэт.",works:["Случаи"],awards:[],places:["Санкт-Петербург"]},
+ {id:"velimir-khlebnikov",fullName:"Велимир Хлебников",birth:"9 ноября 1885",country:"Россия",movement:"Футуризм",biography:"Русский поэт Серебряного века.",works:["Зангези"],awards:[],places:["Астрахань"]},
+ {id:"konstantin-balmont",fullName:"Константин Бальмонт",birth:"15 июня 1867",country:"Россия",movement:"Символизм",biography:"Русский поэт Серебряного века.",works:["Будем как солнце"],awards:[],places:["Москва"]},
+ {id:"valery-bryusov",fullName:"Валерий Брюсов",birth:"13 декабря 1873",country:"Россия",movement:"Символизм",biography:"Русский поэт и писатель.",works:["Юпитер поверженный"],awards:[],places:["Москва"]},
+ {id:"nikolay-ostrovsky",fullName:"Николай Островский",birth:"29 сентября 1904",country:"Россия",movement:"Социалистический реализм",biography:"Русский советский писатель.",works:["Как закалялась сталь"],awards:[],places:["Шепетовка"]},
+ {id:"vasily-grossman",fullName:"Василий Семёнович Гроссман",birth:"12 декабря 1905",country:"Россия",movement:"Реализм",biography:"Русский писатель XX века.",works:["Жизнь и судьба"],awards:[],places:["Бердичев"]}
+];
