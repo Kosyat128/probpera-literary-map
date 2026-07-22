a30fec2d61b8b92ee1856bdf294cbf7299e4f5f7
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 01:40:52 +0300

    Add Aleksandr Solzhenitsyn profile

---
+++ src/data/writers/russia/solzhenitsyn.ts
@@ -0,0 +1,2 @@
+import type { WriterProfile } from "../types";
+export const solzhenitsyn:WriterProfile={id:"alexander-solzhenitsyn",fullName:"Александр Исаевич Солженицын",birth:"11 декабря 1918",death:"3 августа 2008",country:"Россия",movement:"Современная литература",biography:"Русский писатель, лауреат Нобелевской премии по литературе.",works:["Архипелаг ГУЛАГ","Один день Ивана Денисовича","Раковый корпус"],awards:["Нобелевская премия по литературе (1970)"],places:["Москва","Рязань"]};
