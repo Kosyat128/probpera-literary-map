89315f16628a1f0525c8cb5fc79a29035cf471ee
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 00:54:50 +0300

    Create LiteraryMap countries data foundation

---
+++ src/data/literaryMap/countries.ts
@@ -0,0 +1,59 @@
+export const literaryCountries = {
+  russia: {
+    name: "Россия",
+    writers: 127,
+    articles: 127,
+    places: 45,
+    authors: [
+      "Лев Николаевич Толстой",
+      "Фёдор Михайлович Достоевский",
+      "Антон Павлович Чехов"
+    ]
+  },
+
+  france: {
+    name: "Франция",
+    writers: 96,
+    articles: 51,
+    places: 30,
+    authors: [
+      "Виктор Мари Гюго",
+      "Оноре де Бальзак",
+      "Гюстав Флобер"
+    ]
+  },
+
+  uk: {
+    name: "Великобритания",
+    writers: 120,
+    articles: 67,
+    places: 40,
+    authors: [
+      "Уильям Шекспир",
+      "Чарльз Диккенс"
+    ]
+  },
+
+  usa: {
+    name: "США",
+    writers: 150,
+    articles: 80,
+    places: 50,
+    authors: [
+      "Эрнест Хемингуэй",
+      "Марк Твен",
+      "Уильям Фолкнер"
+    ]
+  },
+
+  japan: {
+    name: "Япония",
+    writers: 80,
+    articles: 40,
+    places: 25,
+    authors: [
+      "Ясунари Кавабата",
+      "Харуки Мураками"
+    ]
+  }
+};
