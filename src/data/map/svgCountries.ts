2d69743d29381fe40110f24f7a16fc1b3bf62253
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 02:43:23 +0300

    Add SVG country binding data

---
+++ src/data/map/svgCountries.ts
@@ -0,0 +1,13 @@
+export type SvgCountry={
+ id:string;
+ svgId:string;
+ name:string;
+};
+
+export const svgCountries:SvgCountry[]=[
+ {id:'russia',svgId:'RU',name:'Россия'},
+ {id:'france',svgId:'FR',name:'Франция'},
+ {id:'uk',svgId:'GB',name:'Великобритания'},
+ {id:'usa',svgId:'US',name:'США'},
+ {id:'japan',svgId:'JP',name:'Япония'}
+];
