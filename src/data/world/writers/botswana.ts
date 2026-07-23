dfa80648ad6c6ef2b7a427935b89251a9169595a
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:38:57 +0300

    Add Botswana literature writers database

---
+++ src/data/world/writers/botswana.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const botswanaWriters: WorldWriter[] = [
+ {id:"besetsana-dolo",fullName:"Ботсванская литературная традиция",country:"Ботсвана",birth:"XX век",movement:"Африканская литература",works:["Народные рассказы"]},
+ {id:"unity-dow",fullName:"Юнити Доу",country:"Ботсвана",birth:"1959",movement:"Современная проза",works:["Far and Beyon"]}
+];
