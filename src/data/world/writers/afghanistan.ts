6d3f0d2f95876e9484be9d6195effb204bf6e66c
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:50:04 +0300

    Add Afghan literature writers database

---
+++ src/data/world/writers/afghanistan.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const afghanistanWriters: WorldWriter[] = [
+ {id:"khaled-hosseini",fullName:"Халед Хоссейни",country:"Афганистан",birth:"1965",movement:"Современная проза",works:["Бегущий за ветром","Тысяча сияющих солнц"]},
+ {id:"rahman-baba",fullName:"Рахман Баба",country:"Афганистан",birth:"около 1632",movement:"Пуштунская поэзия",works:["Стихотворения"]}
+];
