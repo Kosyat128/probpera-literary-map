b12780b900c9772a7eb858bda10f4f83f0d35594
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:13:06 +0300

    Add African literature writers database

---
+++ src/data/world/writers/africa.ts
@@ -0,0 +1,8 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const africaWriters: WorldWriter[] = [
+ {id:"naguib-mahfouz",fullName:"Нагиб Махфуз",country:"Египет",birth:"1911",movement:"Реализм",works:["Каирская трилогия"]},
+ {id:"chinua-achebe",fullName:"Чинуа Ачебе",country:"Нигерия",birth:"1930",movement:"Постколониальная литература",works:["И всё рушится"]},
+ {id:"nadine-gordimer",fullName:"Надин Гордимер",country:"ЮАР",birth:"1923",movement:"Современная проза",works:["Хранитель"]},
+ {id:"j-m-coetzee",fullName:"Джон Максвелл Кутзее",country:"ЮАР",birth:"1940",movement:"Постмодернизм",works:["Бесчестье"]}
+];
