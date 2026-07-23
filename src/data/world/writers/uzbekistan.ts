3a3e4e94ed2d4bfcc6de34a1a3047a837c510550
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:52:38 +0300

    Add Uzbek literature writers database

---
+++ src/data/world/writers/uzbekistan.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const uzbekistanWriters: WorldWriter[] = [
+ {id:"alisher-navoi",fullName:"Алишер Навои",country:"Узбекистан",birth:"1441",movement:"Классическая восточная литература",works:["Хамса","Лисан ут-Тайр"]},
+ {id:"abdulla-qahhor",fullName:"Абдулла Каххар",country:"Узбекистан",birth:"1907",movement:"Реализм",works:["Рассказы"]},
+ {id:"chulpon",fullName:"Чулпан",country:"Узбекистан",birth:"1897",movement:"Модернизм",works:["Ночь и день"]}
+];
