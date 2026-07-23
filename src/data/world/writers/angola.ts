b701835cb0d8a77083566d9059099231a2d10cdb
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:39:34 +0300

    Add Angolan literature writers database

---
+++ src/data/world/writers/angola.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const angolaWriters: WorldWriter[] = [
+ {id:"pepete",fullName:"Пепетела",country:"Ангола",birth:"1941",movement:"Постколониальная литература",works:["Майомбе"]},
+ {id:"agostinho-neto",fullName:"Агостиньо Нето",country:"Ангола",birth:"1922",movement:"Африканская поэзия",works:["Священная надежда"]}
+];
