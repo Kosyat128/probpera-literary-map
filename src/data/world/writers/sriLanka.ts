54eb65b0ff90f37f7a4e571869ec6bfe9472666e
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 04:01:21 +0300

    Add Sri Lankan literature writers database

---
+++ src/data/world/writers/sriLanka.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const sriLankaWriters: WorldWriter[] = [
+ {id:"martin-wickramasinghe",fullName:"Мартин Викрамасингхе",country:"Шри-Ланка",birth:"1890",movement:"Современная сингальская литература",works:["Гампералия"]},
+ {id:"ediriweera-sarachchandra",fullName:"Эдиривира Сараччандра",country:"Шри-Ланка",birth:"1914",movement:"Драма",works:["Мановамая"]}
+];
