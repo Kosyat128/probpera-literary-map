690e62facc15c2ee36338a44975179da37bd0401
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:55:22 +0300

    Add Indonesian literature writers database

---
+++ src/data/world/writers/indonesia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const indonesiaWriters: WorldWriter[] = [
+ {id:"pramoedya-ananta-toer",fullName:"Прамудья Ананта Тур",country:"Индонезия",birth:"1925",movement:"Современная проза",works:["Четыре поколения"]},
+ {id:"chairil-anwar",fullName:"Чаирил Анвар",country:"Индонезия",birth:"1922",movement:"Современная поэзия",works:["Я"]}
+];
