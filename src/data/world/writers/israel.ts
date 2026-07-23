ec64a51078aa28f543e17309790ba8c941c07cbe
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:27:13 +0300

    Add Israeli literature writers database

---
+++ src/data/world/writers/israel.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const israelWriters: WorldWriter[] = [
+ {id:"shmuel-agnon",fullName:"Шмуэль Йосеф Агнон",country:"Израиль",birth:"1888",movement:"Современная еврейская литература",works:["Свадебный балдахин"]},
+ {id:"amos-oz",fullName:"Амос Оз",country:"Израиль",birth:"1939",movement:"Современная проза",works:["Мой Михаэль","Повесть о любви и тьме"]},
+ {id:"david-grossman",fullName:"Давид Гроссман",country:"Израиль",birth:"1954",movement:"Современная литература",works:["До конца земли"]}
+];
