ed9a3d24914f587654e7a13ed6497c7e2ef70db9
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:08:15 +0300

    Add Turkish literature writers database

---
+++ src/data/world/writers/turkey.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const turkeyWriters: WorldWriter[] = [
+ {id:"nazim-hikmet",fullName:"Назым Хикмет",country:"Турция",birth:"1902",movement:"Модернизм",works:["Человеческий пейзаж"]},
+ {id:"orhan-pamuk",fullName:"Орхан Памук",country:"Турция",birth:"1952",movement:"Постмодернизм",works:["Меня зовут Красный","Снег"]},
+ {id:"yasar-kemal",fullName:"Яшар Кемаль",country:"Турция",birth:"1923",movement:"Социальная проза",works:["Тонкий Мемед"]}
+];
