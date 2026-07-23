9ebe2385a0fe00d84c816e0c6e42e6427b06aea4
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:03:07 +0300

    Add French literature writers database

---
+++ src/data/world/writers/france.ts
@@ -0,0 +1,9 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const franceWriters: WorldWriter[] = [
+ {id:"victor-hugo",fullName:"Виктор Гюго",country:"Франция",birth:"1802",movement:"Романтизм",works:["Отверженные","Собор Парижской Богоматери"]},
+ {id:"honore-de-balzac",fullName:"Оноре де Бальзак",country:"Франция",birth:"1799",movement:"Реализм",works:["Человеческая комедия","Отец Горио"]},
+ {id:"gustave-flaubert",fullName:"Гюстав Флобер",country:"Франция",birth:"1821",movement:"Реализм",works:["Мадам Бовари"]},
+ {id:"marcel-proust",fullName:"Марсель Пруст",country:"Франция",birth:"1871",movement:"Модернизм",works:["В поисках утраченного времени"]},
+ {id:"alexandre-dumas",fullName:"Александр Дюма",country:"Франция",birth:"1802",movement:"Романтизм",works:["Граф Монте-Кристо","Три мушкетёра"]}
+];
