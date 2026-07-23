67828b750b1520eb20fcc9613f3decbcf1f51e11
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 04:02:00 +0300

    Add Cambodian literature writers database

---
+++ src/data/world/writers/cambodia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const cambodiaWriters: WorldWriter[] = [
+ {id:"krom-ngoy",fullName:"Кром Нгой",country:"Камбоджа",birth:"1865",movement:"Кхмерская классическая поэзия",works:["Поэтические наставления"]},
+ {id:"vann-molyvann-literary",fullName:"Современная камбоджийская литературная традиция",country:"Камбоджа",birth:"XX век",movement:"Современная литература",works:["Кхмерская проза"]}
+];
