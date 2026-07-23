cf5920a4c5c35c252b471ecc8bf901ccbc681164
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:05:13 +0300

    Add Spanish literature writers database

---
+++ src/data/world/writers/spain.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const spainWriters: WorldWriter[] = [
+ {id:"miguel-de-cervantes",fullName:"Мигель де Сервантес",country:"Испания",birth:"1547",movement:"Золотой век испанской литературы",works:["Дон Кихот"]},
+ {id:"federico-garcia-lorca",fullName:"Федерико Гарсиа Лорка",country:"Испания",birth:"1898",movement:"Поколение 27 года",works:["Кровавая свадьба","Дом Бернарды Альбы"]},
+ {id:"miguel-de-unamuno",fullName:"Мигель де Унамуно",country:"Испания",birth:"1864",movement:"Поколение 98 года",works:["Туман"]}
+];
