4aea92afe232d6d0d30d781da576b408bf8fb65d
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:59:52 +0300

    Add Central African Republic literature writers database

---
+++ src/data/world/writers/central_african_republic.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const centralAfricanRepublicWriters: WorldWriter[] = [
+ {id:"etienne-goyemide",fullName:"Этьен Гойемиде",country:"Центральноафриканская Республика",birth:"1942",movement:"Франкоязычная африканская литература",works:["Последний выживший"]},
+ {id:"centrafrique-tradition",fullName:"Литературная традиция Центральноафриканской Республики",country:"ЦАР",birth:"XX век",movement:"Фольклор и современная проза",works:["Народные сказания"]}
+];
