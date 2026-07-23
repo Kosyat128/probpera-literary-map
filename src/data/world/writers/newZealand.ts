a7539191a2ae1650d6674fdfb91c7c5177f99d5e
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:13:24 +0300

    Add New Zealand literature writers database

---
+++ src/data/world/writers/newZealand.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const newZealandWriters: WorldWriter[] = [
+ {id:"katherine-mansfield",fullName:"Кэтрин Мэнсфилд",country:"Новая Зеландия",birth:"1888",movement:"Модернизм",works:["Садовая вечеринка","Прелюдия"]},
+ {id:"janet-frame",fullName:"Джанет Фрейм",country:"Новая Зеландия",birth:"1924",movement:"Современная литература",works:["Лицо в воде"]}
+];
