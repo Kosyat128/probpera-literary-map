2ca47b41a73ac2b5fe36a8269fd56e1c72479033
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:55:43 +0300

    Add Malaysian literature writers database

---
+++ src/data/world/writers/malaysia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const malaysiaWriters: WorldWriter[] = [
+ {id:"abdullah-munsyi",fullName:"Абдулла бин Абдул Кадир Мунши",country:"Малайзия",birth:"1796",movement:"Классическая малайская литература",works:["Хикаят Абдуллы"]},
+ {id:"usman-awang",fullName:"Осман Аванг",country:"Малайзия",birth:"1929",movement:"Современная поэзия",works:["Стихотворения"]}
+];
