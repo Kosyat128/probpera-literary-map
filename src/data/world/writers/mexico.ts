8d154416a2d25726e537c85e230d2981cb2a0567
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:38:18 +0300

    Add Mexican literature writers database

---
+++ src/data/world/writers/mexico.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const mexicoWriters: WorldWriter[] = [
+ {id:"sor-juana-ines",fullName:"Сор Хуана Инес де ла Крус",country:"Мексика",birth:"1648",movement:"Барокко",works:["Первое сновидение"]},
+ {id:"octavio-paz",fullName:"Октавио Пас",country:"Мексика",birth:"1914",movement:"Модернизм",works:["Лабиринт одиночества","Солнечный камень"]},
+ {id:"juan-rulfo",fullName:"Хуан Рульфо",country:"Мексика",birth:"1917",movement:"Магический реализм",works:["Педро Парамо"]}
+];
