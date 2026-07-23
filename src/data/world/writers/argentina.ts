c410d6a9e87d795bfc95ce7626b593d087e7d63d
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:27:30 +0300

    Add Argentine literature writers database

---
+++ src/data/world/writers/argentina.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const argentinaWriters: WorldWriter[] = [
+ {id:"jorge-luis-borges-arg",fullName:"Хорхе Луис Борхес",country:"Аргентина",birth:"1899",movement:"Магический реализм",works:["Вымыслы","Алеф"]},
+ {id:"julio-cortazar",fullName:"Хулио Кортасар",country:"Аргентина",birth:"1914",movement:"Латиноамериканский бум",works:["Игра в классики"]},
+ {id:"ernesto-sabato",fullName:"Эрнесто Сабато",country:"Аргентина",birth:"1911",movement:"Экзистенциализм",works:["Туннель"]}
+];
