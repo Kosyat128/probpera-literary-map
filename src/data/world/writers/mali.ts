89a185a7a151742dc53235bf560aeac27269f267
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:44:12 +0300

    Add Malian literature writers database

---
+++ src/data/world/writers/mali.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const maliWriters: WorldWriter[] = [
+ {id:"amadou-hampate-ba",fullName:"Амаду Хампате Ба",country:"Мали",birth:"1901",movement:"Африканская устная традиция",works:["Странная судьба Вангрена"]},
+ {id:"massa-makan-diabate",fullName:"Масса Макан Диабате",country:"Мали",birth:"1938",movement:"Современная африканская литература",works:["Лейтенант Кейта"]}
+];
