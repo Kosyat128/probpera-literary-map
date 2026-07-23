66a02cc805834b6c40704b5badc7dec6daa5d3c3
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:28:36 +0300

    Add Belarusian literature writers database

---
+++ src/data/world/writers/belarus.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const belarusWriters: WorldWriter[] = [
+ {id:"yanka-kupala",fullName:"Янка Купала",country:"Беларусь",birth:"1882",movement:"Национальное возрождение",works:["Тутэйшыя"]},
+ {id:"yakub-kolas",fullName:"Якуб Колас",country:"Беларусь",birth:"1882",movement:"Реализм",works:["Новая земля"]},
+ {id:"svetlana-aleksievich",fullName:"Светлана Алексиевич",country:"Беларусь",birth:"1948",movement:"Документальная проза",works:["У войны не женское лицо"]}
+];
