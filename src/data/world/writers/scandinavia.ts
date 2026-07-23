ce876f3bb1df31149d094ec03da56149a368e87a
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:22:55 +0300

    Add Scandinavian literature writers database

---
+++ src/data/world/writers/scandinavia.ts
@@ -0,0 +1,8 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const scandinaviaWriters: WorldWriter[] = [
+ {id:"hans-christian-andersen",fullName:"Ханс Кристиан Андерсен",country:"Дания",birth:"1805",movement:"Романтизм",works:["Русалочка","Снежная королева"]},
+ {id:"knut-hamsun",fullName:"Кнут Гамсун",country:"Норвегия",birth:"1859",movement:"Модернизм",works:["Голод","Соки земли"]},
+ {id:"august-strindberg",fullName:"Август Стриндберг",country:"Швеция",birth:"1849",movement:"Натурализм",works:["Красная комната"]},
+ {id:"selma-lagerlof",fullName:"Сельма Лагерлёф",country:"Швеция",birth:"1858",movement:"Романтизм",works:["Сага о Йёсте Берлинге"]}
+];
