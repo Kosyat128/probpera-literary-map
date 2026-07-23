22b0b74e9aea90186cdd6be3c68a41db4e96e83e
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:41:23 +0300

    Add Peruvian literature writers database

---
+++ src/data/world/writers/peru.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const peruWriters: WorldWriter[] = [
+ {id:"mario-vargas-llosa-peru",fullName:"Марио Варгас Льоса",country:"Перу",birth:"1936",movement:"Латиноамериканский бум",works:["Город и псы","Тётушка Хулия и писака"]},
+ {id:"cesar-vallejo",fullName:"Сесар Вальехо",country:"Перу",birth:"1892",movement:"Модернизм",works:["Чёрные вестники"]},
+ {id:"jose-maria-arguedas",fullName:"Хосе Мария Аргедас",country:"Перу",birth:"1911",movement:"Индианизм",works:["Глубокие реки"]}
+];
