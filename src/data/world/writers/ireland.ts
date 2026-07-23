55d39a763387df2bdaf7de6bb4adc71aab206543
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:15:02 +0300

    Add Irish literature writers database

---
+++ src/data/world/writers/ireland.ts
@@ -0,0 +1,8 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const irelandWriters: WorldWriter[] = [
+ {id:"james-joyce",fullName:"Джеймс Джойс",country:"Ирландия",birth:"1882",movement:"Модернизм",works:["Улисс","Портрет художника в юности"]},
+ {id:"oscar-wilde-ireland",fullName:"Оскар Уайльд",country:"Ирландия",birth:"1854",movement:"Эстетизм",works:["Портрет Дориана Грея"]},
+ {id:"w-b-yeats",fullName:"Уильям Батлер Йейтс",country:"Ирландия",birth:"1865",movement:"Ирландское литературное возрождение",works:["Башня","Византия"]},
+ {id:"samuel-beckett",fullName:"Сэмюэл Беккет",country:"Ирландия",birth:"1906",movement:"Театр абсурда",works:["В ожидании Годо"]}
+];
