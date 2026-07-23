7937d95a0a57e2354d9ded4b44ae7e190692d248
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:24:35 +0300

    Add UAE literature writers database

---
+++ src/data/world/writers/uae.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const uaeWriters: WorldWriter[] = [
+ {id:"sultan-al-ameemi",fullName:"Султан аль-Амими",country:"ОАЭ",birth:"1974",movement:"Современная арабская литература",works:["Исследования и проза"]},
+ {id:"ibrahim-al-mulla",fullName:"Ибрахим аль-Мулла",country:"ОАЭ",birth:"XX век",movement:"Современная поэзия",works:["Поэтические сборники"]}
+];
