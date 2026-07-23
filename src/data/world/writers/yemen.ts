e580a9764e7e09ba415d4e1787606fee89c91b41
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:22:56 +0300

    Add Yemeni literature writers database

---
+++ src/data/world/writers/yemen.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const yemenWriters: WorldWriter[] = [
+ {id:"abdulaziz-almaqaleh",fullName:"Абдель Азиз аль-Макалех",country:"Йемен",birth:"1937",movement:"Современная арабская поэзия",works:["Поэтические сборники"]},
+ {id:"ahmad-al-mudah",fullName:"Ахмад аль-Мудах",country:"Йемен",birth:"XX век",movement:"Современная литература",works:["Проза"]}
+];
