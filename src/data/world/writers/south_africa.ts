e611a6468ff195f7e707f10e9ee738d26f4c1240
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:28:35 +0300

    Add South African literature writers database

---
+++ src/data/world/writers/south_africa.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const southAfricaWriters: WorldWriter[] = [
+ {id:"nadine-gordimer",fullName:"Надин Гордимер",country:"ЮАР",birth:"1923",movement:"Антиапартеидная литература",works:["Хранительница"]},
+ {id:"j-m-coetzee",fullName:"Джон Максвелл Кутзее",country:"ЮАР",birth:"1940",movement:"Постмодернизм",works:["Бесчестье","Жизнь и время Михаэла К"]},
+ {id:"athol-fugard",fullName:"Атол Фугард",country:"ЮАР",birth:"1932",movement:"Драматургия",works:["Мастер Гарольд"]}
+];
