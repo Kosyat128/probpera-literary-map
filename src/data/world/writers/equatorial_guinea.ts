b65b3dea9fb494d4430a05923194f93dd7863577
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:59:29 +0300

    Add Equatorial Guinea literature writers database

---
+++ src/data/world/writers/equatorial_guinea.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const equatorialGuineaWriters: WorldWriter[] = [
+ {id:"juan-tomas-avila-laurel",fullName:"Хуан Томас Авила Лаурель",country:"Экваториальная Гвинея",birth:"1966",movement:"Современная африканская литература",works:["Песни черепах"]},
+ {id:"donato-ndongo",fullName:"Донато Ндонго",country:"Экваториальная Гвинея",birth:"1950",movement:"Испаноязычная африканская проза",works:["Гамбоа"]}
+];
