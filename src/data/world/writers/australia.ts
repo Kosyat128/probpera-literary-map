df96ddcf037220416b286476c7762aec56710648
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:12:43 +0300

    Add Australian literature writers database

---
+++ src/data/world/writers/australia.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const australiaWriters: WorldWriter[] = [
+ {id:"patrick-white",fullName:"Патрик Уайт",country:"Австралия",birth:"1912",movement:"Модернизм",works:["Древо человеческое","Виват, Квинт"]},
+ {id:"colleen-mccullough",fullName:"Колин Маккалоу",country:"Австралия",birth:"1937",movement:"Историческая проза",works:["Поющие в терновнике"]},
+ {id:"christina-stevenson",fullName:"Кристина Стед",country:"Австралия",birth:"1902",movement:"Модернизм",works:["Человек, который любил детей"]}
+];
