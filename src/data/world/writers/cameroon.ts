06869e0a36573d2ac50ee59867e09ec363a25cb0
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:43:50 +0300

    Add Cameroon literature writers database

---
+++ src/data/world/writers/cameroon.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const cameroonWriters: WorldWriter[] = [
+ {id:"mongo-beti",fullName:"Монго Бети",country:"Камерун",birth:"1932",movement:"Постколониальная литература",works:["Миссия закончена"]},
+ {id:"calixte-beyala",fullName:"Каликст Бейала",country:"Камерун",birth:"1961",movement:"Современная франкоязычная проза",works:["Дети заката"]}
+];
