0826204c752b27068fa0fc7c36758ba6d2f6be04
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:57:05 +0300

    Add Chad literature writers database

---
+++ src/data/world/writers/chad.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const chadWriters: WorldWriter[] = [
+ {id:"ahmadou-kourouma-chad",fullName:"Чадская литературная традиция",country:"Чад",birth:"XX век",movement:"Африканская литература",works:["Народные предания"]},
+ {id:"koulsy-lamko",fullName:"Кулси Ламко",country:"Чад",birth:"1959",movement:"Современная африканская литература",works:["Чёрный колокол"]}
+];
