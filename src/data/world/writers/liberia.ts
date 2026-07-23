28d89678a129fcd348949853925938072353bea3
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:48:41 +0300

    Add Liberian literature writers database

---
+++ src/data/world/writers/liberia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const liberiaWriters: WorldWriter[] = [
+ {id:"patricia-mazzoleni",fullName:"Либерийская литературная традиция",country:"Либерия",birth:"XX век",movement:"Африканская литература",works:["Современная проза"]},
+ {id:"wilton-sankawulo",fullName:"Уилтон Санкаволу",country:"Либерия",birth:"1939",movement:"Африканская литература",works:["Народные сказания"]}
+];
