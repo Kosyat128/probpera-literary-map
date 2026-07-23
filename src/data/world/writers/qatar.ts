3efefa4cce4e95593e46055db958a4db37cc685b
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:23:15 +0300

    Add Qatari literature writers database

---
+++ src/data/world/writers/qatar.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const qatarWriters: WorldWriter[] = [
+ {id:"abdulrahman-almannaai",fullName:"Абдельрахман аль-Маннаи",country:"Катар",birth:"1948",movement:"Современная арабская литература",works:["Пьесы и рассказы"]},
+ {id:"khalifa-al-majalli",fullName:"Халифа аль-Маджали",country:"Катар",birth:"XX век",movement:"Современная литература",works:["Поэзия"]}
+];
