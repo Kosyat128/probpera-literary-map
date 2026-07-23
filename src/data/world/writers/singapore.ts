fde816dc231889d1d52062d6b6e114f5c7fea81d
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:59:45 +0300

    Add Singapore literature writers database

---
+++ src/data/world/writers/singapore.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const singaporeWriters: WorldWriter[] = [
+ {id:"tan-swee-hian",fullName:"Тан Суэй Хиан",country:"Сингапур",birth:"1943",movement:"Современная литература",works:["Поэзия и проза"]},
+ {id:"alfian-saat",fullName:"Алфиан Саат",country:"Сингапур",birth:"1977",movement:"Современная литература",works:["Коридор"]}
+];
