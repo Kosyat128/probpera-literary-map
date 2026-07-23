84a9a3333c688a3a0ab1e37594c379ab924c689e
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:41:41 +0300

    Add Rwanda literature writers database

---
+++ src/data/world/writers/rwanda.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const rwandaWriters: WorldWriter[] = [
+ {id:"scholastique-mukasonga",fullName:"Шоластик Мукасонга",country:"Руанда",birth:"1956",movement:"Современная африканская проза",works:["Босоногая женщина"]},
+ {id:"simon-gikandi",fullName:"Саймон Гиканди",country:"Руанда",birth:"XX век",movement:"Африканская литературная критика",works:["Исследования литературы"]}
+];
