dc3a0e03d8b3a73add8aa346cf6802b85e34b093
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:56:19 +0300

    Add Togolese literature writers database

---
+++ src/data/world/writers/togo.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const togoWriters: WorldWriter[] = [
+ {id:"fily-dabo-sissoko-togo",fullName:"Тоголезская литературная традиция",country:"Того",birth:"XX век",movement:"Африканская литература",works:["Народные сказания"]},
+ {id:"togo-modern-authors",fullName:"Современные авторы Того",country:"Того",birth:"XX-XXI век",movement:"Современная проза",works:["Рассказы"]}
+];
