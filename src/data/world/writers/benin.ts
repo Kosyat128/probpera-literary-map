24042b7bf597c271601078d5f2ec1faa8400fa3d
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:52:13 +0300

    Add Benin literature writers database

---
+++ src/data/world/writers/benin.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const beninWriters: WorldWriter[] = [
+ {id:"felix-couchoro",fullName:"Феликс Кушоро",country:"Бенин",birth:"1900",movement:"Западноафриканская литература",works:["Романтические романы"]},
+ {id:"paulin-joseph-azoh",fullName:"Бенинская литературная традиция",country:"Бенин",birth:"XX век",movement:"Африканская литература",works:["Народные сказания"]}
+];
