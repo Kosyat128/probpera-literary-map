fabeff65c3dbef9c388b8d233ce116a1d5d7d88f
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:33:22 +0300

    Add Senegalese literature writers database

---
+++ src/data/world/writers/senegal.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const senegalWriters: WorldWriter[] = [
+ {id:"leopold-sedar-senghor",fullName:"Леопольд Седар Сенгор",country:"Сенегал",birth:"1906",movement:"Негритюд",works:["Песни в сумерках"]},
+ {id:"ken-bugul",fullName:"Кен Бугуль",country:"Сенегал",birth:"1947",movement:"Современная африканская литература",works:["Монастыри"]}
+];
