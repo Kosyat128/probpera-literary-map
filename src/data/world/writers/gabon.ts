77427fa4377d6f52ee6335c4fa67fa7ad784e653
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:51:39 +0300

    Add Gabon literature writers database

---
+++ src/data/world/writers/gabon.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const gabonWriters: WorldWriter[] = [
+ {id:"laurent-owondo",fullName:"Лоран Овондо",country:"Габон",birth:"1960",movement:"Современная африканская проза",works:["Зеркало душ"]},
+ {id:"raphael-tony-owono",fullName:"Рафаэль Тони Овоно",country:"Габон",birth:"XX век",movement:"Габонская литература",works:["Проза"]}
+];
