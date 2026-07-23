71760b196b10af25a56119c98a29b117882e2e07
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:41:56 +0300

    Add Chilean literature writers database

---
+++ src/data/world/writers/chile.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const chileWriters: WorldWriter[] = [
+ {id:"pablo-neruda-chile",fullName:"Пабло Неруда",country:"Чили",birth:"1904",movement:"Поэзия XX века",works:["Двадцать стихотворений о любви и одна песня отчаяния"]},
+ {id:"isabel-allende-chile",fullName:"Исабель Альенде",country:"Чили",birth:"1942",movement:"Магический реализм",works:["Дом духов"]},
+ {id:"roberto-bolano",fullName:"Роберто Боланьо",country:"Чили",birth:"1953",movement:"Постмодернизм",works:["2666","Дикие сыщики"]}
+];
