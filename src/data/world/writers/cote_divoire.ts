d604336868677782e42e93c15dc6523cc000b80c
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:44:32 +0300

    Add Ivory Coast literature writers database

---
+++ src/data/world/writers/cote_divoire.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const coteDIvoireWriters: WorldWriter[] = [
+ {id:"bernard-dadié",fullName:"Бернар Бенан Дади",country:"Кот-д’Ивуар",birth:"1916",movement:"Африканская литература",works:["Климат"]},
+ {id:"ahmadou-kourouma",fullName:"Ахмаду Курума",country:"Кот-д’Ивуар",birth:"1927",movement:"Современная африканская проза",works:["Алах не обязан"]}
+];
