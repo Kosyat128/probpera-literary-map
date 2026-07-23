623f84ac7c803b26ead0971bd00265a86cfe3fe1
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:54:55 +0300

    Add Philippine literature writers database

---
+++ src/data/world/writers/philippines.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const philippinesWriters: WorldWriter[] = [
+ {id:"jose-rizal",fullName:"Хосе Рисаль",country:"Филиппины",birth:"1861",movement:"Национальная литература",works:["Не прикасайся ко мне","Флибустьеры"]},
+ {id:"f-sionil-jose",fullName:"Ф. Сионил Хосе",country:"Филиппины",birth:"1924",movement:"Современная проза",works:["Сага о Росалесе"]}
+];
