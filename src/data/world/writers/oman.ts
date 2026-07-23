fab94625722b71f4e3fbaaafd8fc9fbb7985fe9f
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:22:37 +0300

    Add Omani literature writers database

---
+++ src/data/world/writers/oman.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const omanWriters: WorldWriter[] = [
+ {id:"abu-muslim-al-bahlani",fullName:"Абу Муслим аль-Бахлани",country:"Оман",birth:"1860",movement:"Арабская классическая поэзия",works:["Поэтические произведения"]},
+ {id:"oman-modern-literature",fullName:"Современная оманская литературная традиция",country:"Оман",birth:"XX век",movement:"Современная арабская литература",works:["Современная проза"]}
+];
