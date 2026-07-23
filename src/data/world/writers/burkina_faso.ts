6885932891021473d6fb6cee5ed30dbf217b0af6
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:56:44 +0300

    Add Burkina Faso literature writers database

---
+++ src/data/world/writers/burkina_faso.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const burkinaFasoWriters: WorldWriter[] = [
+ {id:"nazi-boni",fullName:"Нази Бони",country:"Буркина-Фасо",birth:"1909",movement:"Африканская литература",works:["Сумерки империй"]},
+ {id:"filippe-oumalo",fullName:"Современная литературная традиция Буркина-Фасо",country:"Буркина-Фасо",birth:"XX век",movement:"Современная литература",works:["Поэзия и проза"]}
+];
