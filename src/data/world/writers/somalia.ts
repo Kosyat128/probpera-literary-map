9a251d3bae4b75f59430bb596988e3a20558d252
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:49:10 +0300

    Add Somali literature writers database

---
+++ src/data/world/writers/somalia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const somaliaWriters: WorldWriter[] = [
+ {id:"nuruddin-farah",fullName:"Нуриддин Фарах",country:"Сомали",birth:"1945",movement:"Постколониальная литература",works:["Изгибы ребра"]},
+ {id:"maxamed-ibrahim-warsame",fullName:"Мохамед Ибрахим Варсаме (Хадрави)",country:"Сомали",birth:"1943",movement:"Сомалийская поэзия",works:["Поэтические произведения"]}
+];
