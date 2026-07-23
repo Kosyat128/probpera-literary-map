27af8b1b7403b003b5de9d59b7e4eef9fa080a80
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 04:08:17 +0300

    Add Laotian literature writers database

---
+++ src/data/world/writers/laos.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const laosWriters: WorldWriter[] = [
+ {id:"pha-lak-pha-lam",fullName:"Лаосская классическая литературная традиция",country:"Лаос",birth:"XIX век",movement:"Народная литература",works:["Пхра Лак Пхра Лам"]},
+ {id:"sithong-khamsavath",fullName:"Ситхонг Кхамсават",country:"Лаос",birth:"XX век",movement:"Современная литература",works:["Лаосская проза"]}
+];
