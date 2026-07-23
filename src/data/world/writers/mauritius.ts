f2c66609180440a8eb75d448938fcbd39e7ebef5
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 04:08:58 +0300

    Add Mauritian literature writers database

---
+++ src/data/world/writers/mauritius.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const mauritiusWriters: WorldWriter[] = [
+ {id:"malcolm-de-chazal",fullName:"Малькольм де Шазаль",country:"Маврикий",birth:"1902",movement:"Философская поэзия",works:["Сенса-Планет"]},
+ {id:"dev-virahsawmy",fullName:"Дев Вирахсавми",country:"Маврикий",birth:"1945",movement:"Современная литература",works:["Креольская драматургия"]}
+];
