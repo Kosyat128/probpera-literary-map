6fea2f0c3f3e081aab138ed4b0251aa07ed80956
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:47:01 +0300

    Add Eswatini literature writers database

---
+++ src/data/world/writers/swaziland.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const eswatiniWriters: WorldWriter[] = [
+ {id:"benedict-wallet-vilakazi",fullName:"Бенедикт Вилakazi",country:"Эсватини / Южная Африка",birth:"1906",movement:"Южноафриканская литература",works:["Поэзия"]},
+ {id:"eswatini-folk-literature",fullName:"Свази́йская устная традиция",country:"Эсватини",birth:"традиция",movement:"Фольклор",works:["Народные сказания"]}
+];
