6ec283f17e6b9ae8eb80562fb66db23f9ac352a0
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:18:20 +0300

    Add Syrian literature writers database

---
+++ src/data/world/writers/syria.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const syriaWriters: WorldWriter[] = [
+ {id:"nizar-qabbani-syria",fullName:"Низар Каббани",country:"Сирия",birth:"1923",movement:"Современная арабская поэзия",works:["Любовные стихотворения"]},
+ {id:"zakaria-tamer",fullName:"Закария Тамер",country:"Сирия",birth:"1931",movement:"Современная проза",works:["Тигры на десятый день"]}
+];
