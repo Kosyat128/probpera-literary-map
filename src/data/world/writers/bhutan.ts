d274677f055414cbc8bdf9df450026d393622947
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:15:18 +0300

    Add Bhutan literature writers database

---
+++ src/data/world/writers/bhutan.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const bhutanWriters: WorldWriter[] = [
+ {id:"dzongsar-khyentse",fullName:"Дзонгсар Кхьенце",country:"Бутан",birth:"1961",movement:"Современная литература",works:["Путешествия с монахом"]},
+ {id:"bhutan-folk-literature",fullName:"Бутанская народная литературная традиция",country:"Бутан",birth:"традиция",movement:"Фольклор",works:["Народные сказания"]}
+];
