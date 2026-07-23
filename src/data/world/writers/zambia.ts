317860bc8c8802db11782f9801ce2f9301676c9c
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:42:05 +0300

    Add Zambian literature writers database

---
+++ src/data/world/writers/zambia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const zambiaWriters: WorldWriter[] = [
+ {id:"binwell-sinyangwe",fullName:"Бинвелл Синьянгве",country:"Замбия",birth:"1956",movement:"Современная африканская проза",works:["Земля предков"]},
+ {id:"ellen-banda-akanja",fullName:"Эллен Банда-Аку",country:"Замбия",birth:"XX век",movement:"Современная литература",works:["Проза"]}
+];
