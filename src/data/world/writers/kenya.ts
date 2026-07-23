64e056f32f9c5184aa378eabef7b7bb1cdbdadbe
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:27:11 +0300

    Add Kenyan literature writers database

---
+++ src/data/world/writers/kenya.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const kenyaWriters: WorldWriter[] = [
+ {id:"ngugi-wa-thiongo",fullName:"Нгуги Ва Тхионго",country:"Кения",birth:"1938",movement:"Постколониальная литература",works:["Зерно пшеницы","Не плачь, дитя"]},
+ {id:"meja-mwangi",fullName:"Мейя Мванги",country:"Кения",birth:"1948",movement:"Современная проза",works:["Убийца"]}
+];
