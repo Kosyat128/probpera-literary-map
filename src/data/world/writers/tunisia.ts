71677df011dfd78a4bb11900b78c41488f8b1501
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:25:18 +0300

    Add Tunisian literature writers database

---
+++ src/data/world/writers/tunisia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const tunisiaWriters: WorldWriter[] = [
+ {id:"abu-l-qasim-al-shabbi",fullName:"Абуль-Касим аш-Шабби",country:"Тунис",birth:"1909",movement:"Арабский романтизм",works:["Песнь жизни"]},
+ {id:"habib-selim",fullName:"Хабиб Селим",country:"Тунис",birth:"XX век",movement:"Современная литература",works:["Проза"]}
+];
