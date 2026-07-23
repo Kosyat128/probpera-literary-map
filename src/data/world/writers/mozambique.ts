ad970383002bd3626b6d4044970aa4a0cc34ab51
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:39:16 +0300

    Add Mozambican literature writers database

---
+++ src/data/world/writers/mozambique.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const mozambiqueWriters: WorldWriter[] = [
+ {id:"mia-couto",fullName:"Мия Коуту",country:"Мозамбик",birth:"1955",movement:"Постколониальная литература",works:["Сонная река","Под фрасковым деревом"]},
+ {id:"jose-craveirinha",fullName:"Жозе Кравейринья",country:"Мозамбик",birth:"1922",movement:"Африканская поэзия",works:["Хамелеон"]}
+];
