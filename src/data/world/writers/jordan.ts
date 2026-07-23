01d88fa65e4e088b0520d36c0824c87b3a5e7e4f
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:20:21 +0300

    Add Jordan literature writers database

---
+++ src/data/world/writers/jordan.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const jordanWriters: WorldWriter[] = [
+ {id:"taha-hussein-jordan",fullName:"Традиция арабской литературы Иордании",country:"Иордания",birth:"XX век",movement:"Современная арабская литература",works:["Современная проза"]},
+ {id:"mustafa-wahbi-al-tal",fullName:"Мустафа Вахби ат-Таль",country:"Иордания",birth:"1899",movement:"Арабская поэзия",works:["Поэтические произведения"]}
+];
