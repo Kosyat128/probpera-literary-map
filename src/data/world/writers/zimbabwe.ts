7feefb94949012751b13932496d7fa706b658a8e
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:37:03 +0300

    Add Zimbabwe literature writers database

---
+++ src/data/world/writers/zimbabwe.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const zimbabweWriters: WorldWriter[] = [
+ {id:"dambudzo-marechera",fullName:"Дамбадзо Маречера",country:"Зимбабве",birth:"1952",movement:"Африканский модернизм",works:["Дом голода"]},
+ {id:"tsitsi-dangarembga",fullName:"Цици Дангарембга",country:"Зимбабве",birth:"1959",movement:"Современная проза",works:["Нервные состояния"]}
+];
