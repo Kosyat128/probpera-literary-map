d10707645ff075863cb014228675c55e0ca8e6bc
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:16:24 +0300

    Add Moldovan literature writers database

---
+++ src/data/world/writers/moldova.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const moldovaWriters: WorldWriter[] = [
+ {id:"grigore-vieru",fullName:"Григоре Виеру",country:"Молдова",birth:"1935",movement:"Современная поэзия",works:["Стихотворения"]},
+ {id:"ion-druta",fullName:"Ион Друцэ",country:"Молдова",birth:"1928",movement:"Современная проза",works:["Последняя любовь"]}
+];
