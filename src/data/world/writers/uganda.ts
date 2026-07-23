1016d71ad826c1a01db87512d06c17c33fe08e51
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:37:41 +0300

    Add Ugandan literature writers database

---
+++ src/data/world/writers/uganda.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const ugandaWriters: WorldWriter[] = [
+ {id:"okot-pbitek",fullName:"Окот п'Битек",country:"Уганда",birth:"1931",movement:"Африканская поэзия",works:["Песнь Лавино"]},
+ {id:"jennifer-nansubuga-makumbi",fullName:"Дженнифер Нансубуга Макумби",country:"Уганда",birth:"1980",movement:"Современная проза",works:["Кинту"]}
+];
