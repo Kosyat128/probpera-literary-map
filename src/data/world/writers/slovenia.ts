4647e2e0c9b980f430add9a5deb4fb64af7a95a3
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:31:02 +0300

    Add Slovenian literature writers database

---
+++ src/data/world/writers/slovenia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const sloveniaWriters: WorldWriter[] = [
+ {id:"france-preseren",fullName:"Франце Прешерн",country:"Словения",birth:"1800",movement:"Романтизм",works:["Сонетный венец"]},
+ {id:"drago-jancar",fullName:"Драго Янчар",country:"Словения",birth:"1948",movement:"Современная литература",works:["Галерник"]}
+];
