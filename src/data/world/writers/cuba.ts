0825bd24dea5e68d166240b870c4a162825ead2f
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:14:14 +0300

    Add Cuban literature writers database

---
+++ src/data/world/writers/cuba.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const cubaWriters: WorldWriter[] = [
+ {id:"jose-marti",fullName:"Хосе Марти",country:"Куба",birth:"1853",movement:"Модернизм",works:["Свободные стихи","Исмаэльильо"]},
+ {id:"alejo-carpentier",fullName:"Алехо Карпентьер",country:"Куба",birth:"1904",movement:"Магический реализм",works:["Царство земное","Век просвещения"]},
+ {id:"guillermo-cabrera-infante",fullName:"Гильермо Кабрера Инфанте",country:"Куба",birth:"1929",movement:"Современная проза",works:["Три грустных тигра"]}
+];
