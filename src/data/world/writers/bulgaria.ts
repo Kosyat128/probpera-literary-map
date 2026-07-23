522ed2a4089b2b30c4642fda3b15abb0e4d0c28c
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:30:10 +0300

    Add Bulgarian literature writers database

---
+++ src/data/world/writers/bulgaria.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const bulgariaWriters: WorldWriter[] = [
+ {id:"ivan-vazov",fullName:"Иван Вазов",country:"Болгария",birth:"1850",movement:"Национальное возрождение",works:["Под игото"]},
+ {id:"hristo-botev",fullName:"Христо Ботев",country:"Болгария",birth:"1848",movement:"Революционная поэзия",works:["Стихотворения"]},
+ {id:"elia-pelevin",fullName:"Элиас Канетти",country:"Болгария",birth:"1905",movement:"Модернизм",works:["Ослепление"]}
+];
