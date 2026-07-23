d675d4835373ce36bc3a0ac3689b1d69117f4ed6
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:15:17 +0300

    Add Polish literature writers database

---
+++ src/data/world/writers/poland.ts
@@ -0,0 +1,8 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const polandWriters: WorldWriter[] = [
+ {id:"adam-mickiewicz",fullName:"Адам Мицкевич",country:"Польша",birth:"1798",movement:"Романтизм",works:["Пан Тадеуш"]},
+ {id:"henryk-sienkiewicz",fullName:"Генрик Сенкевич",country:"Польша",birth:"1846",movement:"Исторический роман",works:["Камо грядеши","Крестоносцы"]},
+ {id:"wislawa-szymborska",fullName:"Вислава Шимборская",country:"Польша",birth:"1923",movement:"Современная поэзия",works:["Конец и начало"]},
+ {id:"olga-tokarczuk",fullName:"Ольга Токарчук",country:"Польша",birth:"1962",movement:"Современная проза",works:["Бегуны","Правек и другие времена"]}
+];
