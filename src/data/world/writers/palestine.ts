d9ec56c1fc5fedb84ef232de17746c34fdeeec4b
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:18:00 +0300

    Add Palestinian literature writers database

---
+++ src/data/world/writers/palestine.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const palestineWriters: WorldWriter[] = [
+ {id:"mahmoud-darwish-pal",fullName:"Махмуд Дарвиш",country:"Палестина",birth:"1941",movement:"Современная поэзия",works:["Почему ты оставил лошадь одну","Меньше роз"]},
+ {id:"ghassan-kanafani",fullName:"Гассан Канафани",country:"Палестина",birth:"1936",movement:"Современная проза",works:["Люди под солнцем"]}
+];
