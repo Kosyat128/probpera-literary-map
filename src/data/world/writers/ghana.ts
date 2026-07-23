e12206e8623072a7bb122756a350790cad2cb7b5
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:28:54 +0300

    Add Ghanaian literature writers database

---
+++ src/data/world/writers/ghana.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const ghanaWriters: WorldWriter[] = [
+ {id:"ama-ata-aidoo",fullName:"Ама Ата Айду",country:"Гана",birth:"1942",movement:"Постколониальная литература",works:["Изменения"]},
+ {id:"kwame-anthony-appiah",fullName:"Кваме Энтони Аппиа",country:"Гана",birth:"1954",movement:"Философская проза",works:["Космополитизм"]}
+];
