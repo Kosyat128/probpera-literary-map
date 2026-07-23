9b9fc0682cae2be73c3980edf0c2ca15c37f4830
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 04:01:38 +0300

    Add Myanmar literature writers database

---
+++ src/data/world/writers/myanmar.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const myanmarWriters: WorldWriter[] = [
+ {id:"thakin-kodaw-hmaing",fullName:"Такин Кодау Хмайн",country:"Мьянма",birth:"1876",movement:"Национальная литература",works:["Стихотворения"]},
+ {id:"dagon-tar-ya",fullName:"Дагон Тар Я",country:"Мьянма",birth:"1919",movement:"Современная литература",works:["Рассказы"]}
+];
