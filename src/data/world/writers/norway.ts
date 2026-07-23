a1ac7b8fb54faf1edd26553b79a8bf6c1fb0b616
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:33:07 +0300

    Add Norwegian literature writers database

---
+++ src/data/world/writers/norway.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const norwayWriters: WorldWriter[] = [
+ {id:"henrik-ibsen",fullName:"Генрик Ибсен",country:"Норвегия",birth:"1828",movement:"Реализм",works:["Кукольный дом","Пер Гюнт"]},
+ {id:"knut-hamsun-no",fullName:"Кнут Гамсун",country:"Норвегия",birth:"1859",movement:"Модернизм",works:["Голод","Соки земли"]},
+ {id:"jo-nesbo",fullName:"Ю Несбё",country:"Норвегия",birth:"1960",movement:"Детективная литература",works:["Серия о Харри Холе"]}
+];
