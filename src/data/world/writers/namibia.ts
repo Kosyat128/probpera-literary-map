26c853926205eb830ec06c75e30ebef802df2a05
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 12:46:06 +0300

    Add Namibian literature writers database

---
+++ src/data/world/writers/namibia.ts
@@ -0,0 +1,6 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const namibiaWriters: WorldWriter[] = [
+ {id:"nangolo-mbumba-literature",fullName:"Намибийская литературная традиция",country:"Намибия",birth:"XX век",movement:"Африканская литература",works:["Современная проза"]},
+ {id:"omaheke-storytelling",fullName:"Традиция устного повествования гереро",country:"Намибия",birth:"традиция",movement:"Фольклор",works:["Народные сказания"]}
+];
