1ba575afd679995405bb3fe17e972ebef21c3133
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:24:49 +0300

    Add Ukrainian literature writers database

---
+++ src/data/world/writers/ukraine.ts
@@ -0,0 +1,8 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const ukraineWriters: WorldWriter[] = [
+ {id:"taras-shevchenko",fullName:"Тарас Шевченко",country:"Украина",birth:"1814",movement:"Романтизм",works:["Кобзарь"]},
+ {id:"ivan-franko",fullName:"Иван Франко",country:"Украина",birth:"1856",movement:"Реализм",works:["Захар Беркут"]},
+ {id:"lesya-ukrainka",fullName:"Леся Украинка",country:"Украина",birth:"1871",movement:"Модернизм",works:["Лесная песня"]},
+ {id:"mykola-gogol",fullName:"Николай Гоголь",country:"Украина",birth:"1809",movement:"Романтизм",works:["Мёртвые души","Ревизор"]}
+];
