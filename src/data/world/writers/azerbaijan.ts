5986a452281b204a4be77d5c2d676e23a21294ce
Kosyat128 <Kosyat128@gmail.com>
2026-07-23 03:36:42 +0300

    Add Azerbaijani literature writers database

---
+++ src/data/world/writers/azerbaijan.ts
@@ -0,0 +1,7 @@
+import type { WorldWriter } from "../writersDatabase";
+
+export const azerbaijanWriters: WorldWriter[] = [
+ {id:"nizami-ganjavi",fullName:"Низами Гянджеви",country:"Азербайджан",birth:"1141",movement:"Средневековая восточная литература",works:["Хамса"]},
+ {id:"samad-vurgun",fullName:"Самед Вургун",country:"Азербайджан",birth:"1906",movement:"Советская литература",works:["Вагиф"]},
+ {id:"akram-ailisli",fullName:"Акрам Айлисли",country:"Азербайджан",birth:"1937",movement:"Современная проза",works:["Каменные сны"]}
+];
