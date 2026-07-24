83700aeb226e86926eefdb7356367765b7a214ec
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 03:38:40 +0300

    Add writer categories for future filters

---
+++ src/data/writerCategories.ts
@@ -0,0 +1,18 @@
+export const writerCategories = [
+  {
+    id: "classics",
+    name: "Классика",
+  },
+  {
+    id: "nobel",
+    name: "Нобелевские лауреаты",
+  },
+  {
+    id: "modern",
+    name: "Современная литература",
+  },
+  {
+    id: "avant-garde",
+    name: "Авангард и модернизм",
+  },
+];
