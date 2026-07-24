9b755325b072e15202f69ec803970aba74fa388b
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 03:47:42 +0300

    Add literary directions metadata for filters

---
+++ src/data/writerDirections.ts
@@ -0,0 +1,9 @@
+export const writerDirections = [
+  { id: "realism", name: "Реализм" },
+  { id: "romanticism", name: "Романтизм" },
+  { id: "symbolism", name: "Символизм" },
+  { id: "modernism", name: "Модернизм" },
+  { id: "postmodernism", name: "Постмодернизм" },
+  { id: "fantasy", name: "Фэнтези и фантастика" },
+  { id: "detective", name: "Детектив" },
+];
