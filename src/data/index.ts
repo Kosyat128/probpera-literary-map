07731048d69fa129995345460cf21179c1526e7f
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 14:21:31 +0300

    Add central data exports for literary map

---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export { allWriters } from "./writersAll";
+export { writers } from "./writers";
+export { writerCategories } from "./writerCategories";
+export { literaryRegions } from "./regions";
+export { literaryPeriods } from "./literaryPeriods";
+export { writerDirections } from "./writerDirections";
