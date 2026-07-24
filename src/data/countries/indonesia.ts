da4da80028d619fd17e25dff712bfaba641c043b
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 02:54:12 +0300

    Create indonesia.ts

---
+++ src/data/countries/indonesia.ts
@@ -0,0 +1,285 @@
+import type { Country } from "../types";
+
+export const indonesia: Country = {
+  id: "indonesia",
+  name: "Индонезия",
+  code: "id",
+
+  writers: [
+
+    {
+      id: "pramoedya_ananta_toer",
+
+      name: "Прамудья Ананта Тур",
+
+      fullName: "Pramoedya Ananta Toer",
+
+      years: "1925–2006",
+
+      birthDate: "1925-02-06",
+
+      deathDate: "2006-04-30",
+
+      birthPlace: "Блора, Центральная Ява, Индонезия",
+
+      coordinates: {
+        lat: -6.9698,
+        lng: 111.418
+      },
+
+      portrait: "",
+
+      bio: "Индонезийский писатель, один из крупнейших авторов Юго-Восточной Азии XX века. Его произведения посвящены колониальной истории Индонезии, борьбе за свободу и судьбе человека в эпоху политических перемен.",
+
+      works: [
+        "This Earth of Mankind",
+        "Child of All Nations",
+        "Footsteps",
+        "House of Glass"
+      ],
+
+      genres: [
+        "роман",
+        "историческая проза",
+        "социальная литература"
+      ],
+
+      language: "индонезийский",
+
+      nationality: "индонезиец",
+
+      awards: [
+        "Премия Рамона Магсайсая"
+      ],
+
+      relatedWriters: [
+        "hamka",
+        "ayu_utami"
+      ],
+
+      tags: [
+        "XX век",
+        "исторический роман",
+        "мировая литература"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "hamka",
+
+      name: "Хамка",
+
+      fullName: "Haji Abdul Malik Karim Amrullah",
+
+      years: "1908–1981",
+
+      birthDate: "1908-02-17",
+
+      deathDate: "1981-07-24",
+
+      birthPlace: "Манинджау, Западная Суматра, Индонезия",
+
+      coordinates: {
+        lat: -0.3,
+        lng: 100.2
+      },
+
+      portrait: "",
+
+      bio: "Индонезийский писатель, богослов и мыслитель. Автор одного из наиболее известных романов индонезийской литературы XX века.",
+
+      works: [
+        "Tenggelamnya Kapal Van der Wijck",
+        "Di Bawah Lindungan Ka'bah"
+      ],
+
+      genres: [
+        "роман",
+        "религиозная проза"
+      ],
+
+      language: "индонезийский",
+
+      nationality: "индонезиец",
+
+      awards: [],
+
+      relatedWriters: [
+        "pramoedya_ananta_toer"
+      ],
+
+      tags: [
+        "XX век",
+        "классическая литература"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "achdiyat_karta_mihardja",
+
+      name: "Ахдиат Карта Михарджа",
+
+      fullName: "Achdiat Karta Mihardja",
+
+      years: "1911–2010",
+
+      birthDate: "1911-03-06",
+
+      deathDate: "2010-07-08",
+
+      birthPlace: "Гарут, Западная Ява, Индонезия",
+
+      coordinates: {
+        lat: -7.2167,
+        lng: 107.9
+      },
+
+      portrait: "",
+
+      bio: "Индонезийский писатель, классик национальной литературы. Его роман «Атеист» считается одним из важнейших произведений индонезийской прозы.",
+
+      works: [
+        "Atheis"
+      ],
+
+      genres: [
+        "роман",
+        "философская проза"
+      ],
+
+      language: "индонезийский",
+
+      nationality: "индонезиец",
+
+      awards: [],
+
+      relatedWriters: [
+        "pramoedya_ananta_toer",
+        "hamka"
+      ],
+
+      tags: [
+        "XX век",
+        "классика"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "ayu_utami",
+
+      name: "Аю Утами",
+
+      fullName: "Ayu Utami",
+
+      years: "1968–",
+
+      birthDate: "1968-11-21",
+
+      deathDate: "",
+
+      birthPlace: "Богор, Западная Ява, Индонезия",
+
+      coordinates: {
+        lat: -6.5971,
+        lng: 106.806
+      },
+
+      portrait: "",
+
+      bio: "Индонезийская писательница и журналистка. Одна из ведущих фигур современной индонезийской литературы.",
+
+      works: [
+        "Saman",
+        "Larung"
+      ],
+
+      genres: [
+        "роман",
+        "современная проза"
+      ],
+
+      language: "индонезийский",
+
+      nationality: "индонезийка",
+
+      awards: [
+        "Премия имени Принца Клауса"
+      ],
+
+      relatedWriters: [
+        "pramoedya_ananta_toer"
+      ],
+
+      tags: [
+        "XXI век",
+        "современная литература"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "goenawan_mohamad",
+
+      name: "Гоэнтаджара Мохамад",
+
+      fullName: "Goenawan Mohamad",
+
+      years: "1941–",
+
+      birthDate: "1941-07-29",
+
+      deathDate: "",
+
+      birthPlace: "Батанг, Центральная Ява, Индонезия",
+
+      coordinates: {
+        lat: -6.9097,
+        lng: 109.731
+      },
+
+      portrait: "",
+
+      bio: "Индонезийский поэт, эссеист и журналист. Один из крупнейших интеллектуалов современной Индонезии.",
+
+      works: [
+        "Pariksit",
+        "Interlude"
+      ],
+
+      genres: [
+        "поэзия",
+        "эссе"
+      ],
+
+      language: "индонезийский",
+
+      nationality: "индонезиец",
+
+      awards: [],
+
+      relatedWriters: [
+        "ayu_utami"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век",
+        "поэзия"
+      ],
+
+      articleUrl: ""
+    }
+
+  ]
+};
