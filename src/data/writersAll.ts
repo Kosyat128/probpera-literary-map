e1aed1007bdbb03ce9af779edf1a3d4fd6d55218
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 14:27:30 +0300

    Include Russian writers in combined dataset

--- src/data/writersAll.ts
+++ src/data/writersAll.ts
@@ -4,9 +4,11 @@ import { asianWriters } from "./writersAsia";
 import { americanWriters } from "./writersAmerica";
 import { africanWriters } from "./writersAfrica";
 import { oceaniaWriters } from "./writersOceania";
+import { russianWriters } from "./writersRussia";
 
 export const allWriters = [
   ...writers,
+  ...russianWriters,
   ...europeanWriters,
   ...asianWriters,
   ...americanWriters,
