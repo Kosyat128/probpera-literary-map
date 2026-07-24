7f9247aeef88f9877877f35925b55d706a6c73e9
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 02:26:40 +0300

    Add data for Saint Vincent and the Grenadines

---
+++ src/data/countries/saint_vincent_and_the_grenadines.ts
@@ -0,0 +1,44 @@
+import type { Country } from "../types";
+
+export const saintVincentAndTheGrenadines: Country = {
+  id: "saint_vincent_and_the_grenadines",
+  name: "Сент-Винсент и Гренадины",
+  code: "vc",
+  writers: [
+    {
+      id: "michael_anthony",
+      name: "Майкл Энтони",
+      years: "1930–2019",
+      birthDate: "1930-02-10",
+      deathDate: "2019-01-01",
+      birthPlace: "Сент-Винсент",
+      deathPlace: "Тринидад и Тобаго",
+      coordinates: {
+        lat: 13.1600,
+        lng: -61.2248
+      },
+      portrait: "",
+      bio: "Писатель и историк, родившийся в Сент-Винсенте и Гренадинах. Один из заметных авторов Восточных Карибов. В своих произведениях обращался к истории островов, культуре, традициям и жизни жителей Карибского региона.",
+      works: [
+        "The Year in San Fernando",
+        "Green Days by the River",
+        "The Games Were Coming"
+      ],
+      genres: [
+        "роман",
+        "историческая проза",
+        "рассказ"
+      ],
+      language: "английский",
+      nationality: "винсентиец",
+      awards: [],
+      relatedWriters: [],
+      tags: [
+        "XX век",
+        "карибская литература",
+        "англоязычная литература"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
