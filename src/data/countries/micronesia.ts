12d51122d7fb9979c89293835d6d6c26099e1fd6
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 02:52:39 +0300

    Create micronesia.ts

---
+++ src/data/countries/micronesia.ts
@@ -0,0 +1,116 @@
+import type { Country } from "../types";
+
+export const micronesia: Country = {
+  id: "micronesia",
+  name: "Федеративные Штаты Микронезии",
+  code: "fm",
+
+  writers: [
+
+    {
+      id: "peter_sigeo",
+
+      name: "Питер Сигео",
+
+      fullName: "Peter Sigeo",
+
+      years: "1950–",
+
+      birthDate: "1950-01-01",
+
+      deathDate: "",
+
+      birthPlace: "Чуук, Федеративные Штаты Микронезии",
+
+      coordinates: {
+        lat: 7.4256,
+        lng: 151.858
+      },
+
+      portrait: "",
+
+      bio: "Микронезийский писатель и культурный деятель, связанный с развитием письменной культуры Чуука и Федеративных Штатов Микронезии.",
+
+      works: [
+        "Литературные и культурные публикации"
+      ],
+
+      genres: [
+        "проза",
+        "культурная публицистика"
+      ],
+
+      language: "английский",
+
+      nationality: "микронезиец",
+
+      awards: [],
+
+      relatedWriters: [
+        "kathy_jetnil_kijiner",
+        "albert_wendt"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век",
+        "микронезийская литература"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "marcel_mares",
+
+      name: "Марсель Лоус",
+
+      fullName: "Marcel Mares",
+
+      years: "XX–XXI век",
+
+      birthDate: "",
+
+      deathDate: "",
+
+      birthPlace: "Федеративные Штаты Микронезии",
+
+      coordinates: {
+        lat: 6.9248,
+        lng: 158.161
+      },
+
+      portrait: "",
+
+      bio: "Современный автор, связанный с литературным и культурным пространством Федеративных Штатов Микронезии.",
+
+      works: [
+        "Современные литературные публикации"
+      ],
+
+      genres: [
+        "проза",
+        "эссе"
+      ],
+
+      language: "английский",
+
+      nationality: "микронезиец",
+
+      awards: [],
+
+      relatedWriters: [
+        "peter_sigeo"
+      ],
+
+      tags: [
+        "XXI век",
+        "литература Океании"
+      ],
+
+      articleUrl: ""
+    }
+
+  ]
+};
