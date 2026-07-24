1d7c46c0769a85303e96588390bb91e807ef362c
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 02:52:08 +0300

    Create marshall_islands.ts

---
+++ src/data/countries/marshall_islands.ts
@@ -0,0 +1,66 @@
+import type { Country } from "../types";
+
+export const marshallIslands: Country = {
+  id: "marshall_islands",
+  name: "Маршалловы Острова",
+  code: "mh",
+
+  writers: [
+
+    {
+      id: "kathy_jetnil_kijiner",
+
+      name: "Кэти Джетнил-Кидд",
+
+      fullName: "Kathy Jetnil-Kijiner",
+
+      years: "1989–",
+
+      birthDate: "1989-01-01",
+
+      deathDate: "",
+
+      birthPlace: "Маршалловы Острова",
+
+      coordinates: {
+        lat: 7.1164,
+        lng: 171.1858
+      },
+
+      portrait: "",
+
+      bio: "Маршалльская поэтесса, писательница и экологическая активистка. Одна из наиболее известных современных литературных представительниц Маршалловых Островов. В её творчестве важное место занимают темы изменения климата, культурной памяти и идентичности островных народов.",
+
+      works: [
+        "Iep Jāltok: Poems from a Marshallese Daughter",
+        "Dear Matafele Peinem"
+      ],
+
+      genres: [
+        "поэзия",
+        "эссе",
+        "экологическая литература"
+      ],
+
+      language: "английский",
+
+      nationality: "маршаллька",
+
+      awards: [],
+
+      relatedWriters: [
+        "albert_wendt",
+        "tusiata_avia"
+      ],
+
+      tags: [
+        "XXI век",
+        "тихоокеанская литература",
+        "экологическая литература"
+      ],
+
+      articleUrl: ""
+    }
+
+  ]
+};
