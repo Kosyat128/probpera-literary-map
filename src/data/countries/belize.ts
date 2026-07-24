9dc4baff3324caf74a14e46e029c34ccc1c5ae04
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 02:14:50 +0300

    Create belize.ts

---
+++ src/data/countries/belize.ts
@@ -0,0 +1,111 @@
+import type { Country } from "../types";
+
+export const belize: Country = {
+  id: "belize",
+  name: "Белиз",
+  code: "bz",
+  writers: [
+    {
+      id: "zee_edgell",
+      name: "Зи Эджелл",
+      years: "1940–2020",
+      birthDate: "1940-10-21",
+      deathDate: "2020-08-20",
+      birthPlace: "Белиз-Сити, Британский Гондурас",
+      deathPlace: "Белиз",
+      coordinates: {
+        lat: 17.5046,
+        lng: -88.1962
+      },
+      portrait: "",
+      bio: "Белизская писательница, одна из главных фигур литературы страны. Её роман «Beka Lamb» считается одним из важнейших произведений белизской литературы. В творчестве обращалась к вопросам идентичности, колониального наследия и жизни общества Белиза.",
+      works: [
+        "Beka Lamb",
+        "In Times Like These"
+      ],
+      genres: [
+        "роман",
+        "современная проза"
+      ],
+      language: "английский",
+      nationality: "белизка",
+      awards: [],
+      relatedWriters: [
+        "evan_x_hyde",
+        "leonard_s_young"
+      ],
+      tags: [
+        "XX век",
+        "XXI век",
+        "карибская литература"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "evan_x_hyde",
+      name: "Эван X Хайд",
+      years: "1950–",
+      birthDate: "1950-01-01",
+      birthPlace: "Белиз",
+      coordinates: {
+        lat: 17.5046,
+        lng: -88.1962
+      },
+      portrait: "",
+      bio: "Белизский писатель, журналист и общественный деятель. Один из заметных современных авторов страны. В своих произведениях обращается к социальной жизни Белиза, истории и культурной идентичности.",
+      works: [
+        "The Last Flight of the Scarlet Macaw",
+        "Белизские рассказы"
+      ],
+      genres: [
+        "роман",
+        "рассказ",
+        "публицистика"
+      ],
+      language: "английский",
+      nationality: "белизец",
+      awards: [],
+      relatedWriters: [
+        "zee_edgell"
+      ],
+      tags: [
+        "XX век",
+        "XXI век",
+        "современная литература"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "leonard_s_young",
+      name: "Леонард С. Янг",
+      years: "XX век",
+      birthPlace: "Белиз",
+      coordinates: {
+        lat: 17.5046,
+        lng: -88.1962
+      },
+      portrait: "",
+      bio: "Белизский писатель и поэт, представитель национальной литературной традиции страны.",
+      works: [
+        "Поэтические произведения"
+      ],
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+      language: "английский",
+      nationality: "белизец",
+      awards: [],
+      relatedWriters: [
+        "zee_edgell"
+      ],
+      tags: [
+        "XX век",
+        "белизская литература"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
