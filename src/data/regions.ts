85739f104c8b00b83904602ec33b5ec62c001651
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 03:39:13 +0300

    Add literary world regions data structure

---
+++ src/data/regions.ts
@@ -0,0 +1,7 @@
+export const literaryRegions = [
+  { id: "europe", name: "Европа" },
+  { id: "asia", name: "Азия" },
+  { id: "america", name: "Америка" },
+  { id: "africa", name: "Африка" },
+  { id: "oceania", name: "Океания" },
+];
