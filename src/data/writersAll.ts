3631083f0687adf158007c59d8f1e607841c0eef
Kosyat128 <Kosyat128@gmail.com>
2026-07-24 14:42:54 +0300

    Connect Middle East writers dataset

--- src/data/writersAll.ts
+++ src/data/writersAll.ts
@@ -1,10 +1,11 @@
 import { writers } from "./writers";
+import { russianWriters } from "./writersRussia";
 import { europeanWriters } from "./writersEurope";
 import { asianWriters } from "./writersAsia";
 import { americanWriters } from "./writersAmerica";
 import { africanWriters } from "./writersAfrica";
 import { oceaniaWriters } from "./writersOceania";
-import { russianWriters } from "./writersRussia";
+import { middleEastWriters } from "./writersMiddleEast";
 
 export const allWriters = [
   ...writers,
@@ -14,4 +15,5 @@ export const allWriters = [
   ...americanWriters,
   ...africanWriters,
   ...oceaniaWriters,
+  ...middleEastWriters,
 ];
