562e34acfa9adbffca4f124b814ab4fe3806abb7
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:36:25 +0300

    Create djibouti.ts

---
+++ src/data/countries/djibouti.ts
@@ -0,0 +1,89 @@
+import type { Country } from "../types";
+
+export const djibouti: Country = {
+  id: "djibouti",
+  name: "Джибути",
+  code: "dj",
+
+  writers: [
+    {
+      id: "abdourahman_waberi",
+      name: "Абдурахман Али Варсама",
+      fullName: "Abdourahman A. Waberi",
+      years: "1965–",
+      birthDate: "1965-07-20",
+      deathDate: "",
+      birthPlace: "Джибути",
+      portrait: "",
+      bio: "Джибутийский писатель, один из крупнейших представителей франкоязычной литературы Африки.",
+      works: [
+        "Pays sans ombre",
+        "Aux États-Unis d’Afrique",
+        "Cahier nomade"
+      ],
+      genres: [
+        "роман",
+        "рассказ",
+        "эссе"
+      ],
+      language: "французский",
+      nationality: "джибутиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "abdourahman_h_yama",
+      name: "Абдулкадир Хирси Яма",
+      fullName: "Abdourahman H. Yama",
+      years: "1958–",
+      birthDate: "1958",
+      deathDate: "",
+      birthPlace: "Джибути",
+      portrait: "",
+      bio: "Джибутийский писатель и поэт.",
+      works: [],
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+      language: "французский",
+      nationality: "джибутиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "aden_robleh_awaleh",
+      name: "Аден Роблех Авале",
+      fullName: "Aden Robleh Awaleh",
+      years: "1940–",
+      birthDate: "1940",
+      deathDate: "",
+      birthPlace: "Джибути",
+      portrait: "",
+      bio: "Джибутийский автор и участник культурной жизни страны.",
+      works: [],
+      genres: [
+        "проза",
+        "публицистика"
+      ],
+      language: "французский",
+      nationality: "джибутиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
