cf76aab8469bae20b0973aeb25e3b8b4908a622c
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:55:26 +0300

    Create niue.ts

---
+++ src/data/countries/niue.ts
@@ -0,0 +1,38 @@
+import type { Country } from "../types";
+
+export const niue: Country = {
+  id: "niue",
+  name: "Ниуэ",
+  code: "nu",
+
+  writers: [
+    {
+      id: "john_pule",
+      name: "Джон Пуле",
+      fullName: "John Puhiatau Pule",
+      years: "1962–",
+      birthDate: "1962-04-18",
+      deathDate: "",
+      birthPlace: "Алофи, Ниуэ",
+      portrait: "",
+      bio: "Ниуэанский писатель, поэт и художник, один из крупнейших представителей современной литературы островов Тихого океана.",
+      works: [
+        "The Shark That Ate the Sun",
+        "Burn My Head in Heaven"
+      ],
+      genres: [
+        "поэзия",
+        "роман",
+        "проза"
+      ],
+      language: "английский",
+      nationality: "ниуэанец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
