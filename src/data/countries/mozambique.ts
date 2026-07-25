9943807fd4ade4848ac6e31455d4b1ebf78a9981
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 14:02:33 +0300

    Create mozambique.ts

---
+++ src/data/countries/mozambique.ts
@@ -0,0 +1,428 @@
+import type { Country } from "../types";
+
+export const mozambique: Country = {
+  id: "mozambique",
+  name: "Мозамбик",
+  code: "mz",
+
+  writers: [
+    {
+      id: "orlando_mendes",
+      name: "Орланду Мендеш",
+      fullName: "Orlando Mendes",
+      years: "1916–1990",
+      birthDate: "1916-08-04",
+      deathDate: "1990-01-11",
+      birthPlace: "Илья-ди-Мосамбик",
+
+      coordinates: {
+        lat: -15.03,
+        lng: 40.73
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский писатель и поэт, один из первых крупных романистов страны.",
+
+      works: [
+        "Portagem"
+      ],
+
+      genres: [
+        "роман",
+        "поэзия"
+      ],
+
+      language: "португальский",
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "jose_craveirinha"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "jose_craveirinha",
+      name: "Жозе Кравейринья",
+      fullName: "José Craveirinha",
+      years: "1922–2003",
+      birthDate: "1922-05-28",
+      deathDate: "2003-02-06",
+      birthPlace: "Лоренсу-Маркиш (Мапуту)",
+
+      coordinates: {
+        lat: -25.97,
+        lng: 32.58
+      },
+
+      portrait: "",
+
+      bio: "Крупнейший мозамбикский поэт, один из основателей национальной литературы.",
+
+      works: [
+        "Xigubo",
+        "Karingana ua Karingana"
+      ],
+
+      genres: [
+        "поэзия"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [
+        "Премия Камоэнса 1991"
+      ],
+
+      relatedWriters: [
+        "luis_bernardo_honwana"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "luis_bernardo_honwana",
+      name: "Луис Бернарду Хонвана",
+      fullName: "Luís Bernardo Honwana",
+      years: "1942–",
+
+      birthDate: "1942-11-12",
+      deathDate: "",
+
+      birthPlace: "Лоренсу-Маркиш (Мапуту)",
+
+      coordinates: {
+        lat: -25.97,
+        lng: 32.58
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский писатель, автор одного из важнейших произведений африканской португалоязычной литературы.",
+
+      works: [
+        "Мы убили пса Тико"
+      ],
+
+      genres: [
+        "рассказ",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "mia_couto"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "mia_couto",
+      name: "Миа Коуту",
+      fullName: "António Emílio Leite Couto",
+      years: "1955–",
+
+      birthDate: "1955-07-05",
+      deathDate: "",
+
+      birthPlace: "Бейра",
+
+      coordinates: {
+        lat: -19.84,
+        lng: 34.84
+      },
+
+      portrait: "",
+
+      bio: "Один из самых известных современных писателей Мозамбика и португалоязычной Африки.",
+
+      works: [
+        "Последний полёт фламинго",
+        "Сон на пороге"
+      ],
+
+      genres: [
+        "роман",
+        "магический реализм"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [
+        "Премия Камоэнса 2013"
+      ],
+
+      relatedWriters: [
+        "paulina_chiziane"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "paulina_chiziane",
+      name: "Паулина Чизьяне",
+      fullName: "Paulina Chiziane",
+      years: "1955–",
+
+      birthDate: "1955-06-04",
+      deathDate: "",
+
+      birthPlace: "Маниака",
+
+      coordinates: {
+        lat: -25.97,
+        lng: 32.58
+      },
+
+      portrait: "",
+
+      bio: "Первая женщина-писательница Мозамбика, получившая широкое международное признание.",
+
+      works: [
+        "Первое женское"
+      ],
+
+      genres: [
+        "роман",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбичка",
+
+      awards: [
+        "Премия Камоэнса 2021"
+      ],
+
+      relatedWriters: [
+        "ungulani_ba_ka_khossa"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "ungulani_ba_ka_khossa",
+      name: "Унгулани Ба Ка Хоса",
+      fullName: "Ungulani Ba Ka Khossa",
+      years: "1957–",
+
+      birthDate: "1957-08-01",
+      deathDate: "",
+
+      birthPlace: "Инхамбане",
+
+      coordinates: {
+        lat: -23.87,
+        lng: 35.38
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский романист и один из ведущих представителей современной прозы страны.",
+
+      works: [
+        "У призраков есть дом"
+      ],
+
+      genres: [
+        "роман"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "joao_paulo_borges_coelho"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "joao_paulo_borges_coelho",
+      name: "Жуан Паулу Боржеш Коэлью",
+      fullName: "João Paulo Borges Coelho",
+      years: "1955–",
+
+      birthDate: "1955-12-23",
+      deathDate: "",
+
+      birthPlace: "Порту",
+
+      coordinates: {
+        lat: 41.15,
+        lng: -8.61
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский писатель и историк, один из крупнейших современных романистов страны.",
+
+      works: [
+        "Городские хроники"
+      ],
+
+      genres: [
+        "исторический роман",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "nelson_saute"
+      ],
+
+      tags: [
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "nelson_saute",
+      name: "Нелсон Сауте",
+      fullName: "Nelson Saúte",
+      years: "1967–",
+
+      birthDate: "1967-02-26",
+      deathDate: "",
+
+      birthPlace: "Мапуту",
+
+      coordinates: {
+        lat: -25.97,
+        lng: 32.58
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский писатель, поэт и литературный критик.",
+
+      works: [
+        "Письма из Индийского океана"
+      ],
+
+      genres: [
+        "поэзия",
+        "эссе"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "adelino_timoteo"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    },
+
+    {
+      id: "adelino_timoteo",
+      name: "Аделину Тимотеу",
+      fullName: "Adelino Timóteo",
+      years: "1960–",
+
+      birthDate: "1960-04-12",
+      deathDate: "",
+
+      birthPlace: "Келимане",
+
+      coordinates: {
+        lat: -17.88,
+        lng: 36.89
+      },
+
+      portrait: "",
+
+      bio: "Мозамбикский поэт, писатель и журналист.",
+
+      works: [
+        "Наследие крови"
+      ],
+
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "мозамбикец",
+
+      awards: [],
+
+      relatedWriters: [
+        "mia_couto"
+      ],
+
+      tags: [
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    }
+  ]
+};
