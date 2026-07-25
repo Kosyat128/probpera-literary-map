852b3ed6f997e662f0f0e5f99f345a8e41d8b6fd
Kosyat128 <Kosyat128@gmail.com>
2026-07-25 03:36:00 +0300

    Add Timor-Leste country data with writers
    Added data for Timor-Leste including writers and their details.

---
+++ src/data/countries/timor_leste.ts
@@ -0,0 +1,207 @@
+import type { Country } from "../types";
+
+export const timorLeste: Country = {
+  id: "timor_leste",
+  name: "Восточный Тимор",
+  code: "tl",
+
+  writers: [
+
+    {
+      id: "francisco_borja_da_costa",
+
+      name: "Франсишку Боржа да Кошта",
+      fullName: "Francisco Borja da Costa",
+
+      years: "1946–1975",
+
+      birthDate: "1946-10-14",
+      deathDate: "1975-12-07",
+
+      birthPlace: "Дили, Восточный Тимор",
+
+      coordinates: {
+        lat: -8.5569,
+        lng: 125.5603
+      },
+
+      portrait: "",
+
+      bio: "Тиморский поэт, журналист и один из основателей современной литературы Восточного Тимора.",
+
+      works: [
+        "Патриа",
+        "Поэтические произведения"
+      ],
+
+      genres: [
+        "поэзия",
+        "публицистика"
+      ],
+
+      language: "португальский",
+      nationality: "тиморец",
+
+      awards: [],
+
+      relatedWriters: [
+        "luis_cardoso"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "fernando_sylvan",
+
+      name: "Фернанду Силван",
+      fullName: "Fernando Sylvan",
+
+      years: "1917–1993",
+
+      birthDate: "1917-08-26",
+      deathDate: "1993-12-21",
+
+      birthPlace: "Дили, Восточный Тимор",
+
+      coordinates: {
+        lat: -8.5569,
+        lng: 125.5603
+      },
+
+      portrait: "",
+
+      bio: "Поэт тиморского происхождения, один из представителей португалоязычной литературы.",
+
+      works: [
+        "Поэтические сборники",
+        "Эссе"
+      ],
+
+      genres: [
+        "поэзия",
+        "эссе"
+      ],
+
+      language: "португальский",
+
+      nationality: "тиморец",
+
+      awards: [],
+
+      relatedWriters: [
+        "francisco_borja_da_costa"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "jorge_barretto_xavier",
+
+      name: "Хорхе Баррету Ксавьер",
+      fullName: "Jorge Barretto Xavier",
+
+      years: "",
+
+      birthDate: "",
+      deathDate: "",
+
+      birthPlace: "Восточный Тимор",
+
+      coordinates: {
+        lat: -8.5569,
+        lng: 125.5603
+      },
+
+      portrait: "",
+
+      bio: "Автор тиморской португалоязычной литературной традиции.",
+
+      works: [],
+
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "тиморец",
+
+      awards: [],
+
+      relatedWriters: [
+        "fernando_sylvan"
+      ],
+
+      tags: [
+        "XX век"
+      ],
+
+      articleUrl: ""
+    },
+
+
+    {
+      id: "luis_cardoso",
+
+      name: "Луиш Кардозу",
+      fullName: "Luís Cardoso",
+
+      years: "1958–",
+
+      birthDate: "1958-12-07",
+      deathDate: "",
+
+      birthPlace: "Кайколи, Восточный Тимор",
+
+      coordinates: {
+        lat: -8.5569,
+        lng: 125.5603
+      },
+
+      portrait: "",
+
+      bio: "Крупнейший современный писатель Восточного Тимора, автор романов на португальском языке.",
+
+      works: [
+        "Кронология одного путешествия",
+        "Остров обречённых"
+      ],
+
+      genres: [
+        "роман",
+        "проза"
+      ],
+
+      language: "португальский",
+
+      nationality: "тиморец",
+
+      awards: [],
+
+      relatedWriters: [
+        "francisco_borja_da_costa"
+      ],
+
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+
+      articleUrl: ""
+    }
+
+  ]
+};
