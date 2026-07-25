501e63b2b073de17215d3652277a8dd3ce0edc79
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 00:39:43 +0300

    Create uganda.ts

---
+++ src/data/countries/uganda.ts
@@ -0,0 +1,190 @@
+import type { Country } from "../types";
+
+export const uganda: Country = {
+  id: "uganda",
+  name: "Уганда",
+  code: "ug",
+
+  writers: [
+    {
+      id: "okot_pbitek",
+      name: "Окот п’Битек",
+      fullName: "Okot p’Bitek",
+      years: "1931–1982",
+      birthDate: "1931-06-07",
+      deathDate: "1982-07-20",
+      birthPlace: "Гулу",
+      portrait: "",
+      bio: "Угандийский поэт и писатель, один из крупнейших представителей африканской литературы XX века.",
+      works: [
+        "Песнь Лавино",
+        "Песнь Окола"
+      ],
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+      language: "английский",
+      nationality: "угандиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "timothy_wangusa",
+      name: "Тимоти Ваньяра",
+      fullName: "Timothy Wangusa",
+      years: "1942–",
+      birthDate: "1942",
+      deathDate: "",
+      birthPlace: "Уганда",
+      portrait: "",
+      bio: "Угандийский поэт, романист и один из классиков современной литературы страны.",
+      works: [
+        "Узник совести"
+      ],
+      genres: [
+        "роман",
+        "поэзия"
+      ],
+      language: "английский",
+      nationality: "угандиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "mary_karooro_okurut",
+      name: "Мэри Кароро Окурут",
+      fullName: "Mary Karooro Okurut",
+      years: "1954–",
+      birthDate: "1954",
+      deathDate: "",
+      birthPlace: "Уганда",
+      portrait: "",
+      bio: "Угандийская писательница, автор романов о социальной и культурной жизни страны.",
+      works: [
+        "The Invisible Weevil"
+      ],
+      genres: [
+        "роман"
+      ],
+      language: "английский",
+      nationality: "угандийка",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "jennifer_nansubuga_makumbi",
+      name: "Дженифер Нансубуга Макаумби",
+      fullName: "Jennifer Nansubuga Makumbi",
+      years: "1967–",
+      birthDate: "1967",
+      deathDate: "",
+      birthPlace: "Кампала",
+      portrait: "",
+      bio: "Одна из наиболее известных современных писательниц Уганды.",
+      works: [
+        "Кинту"
+      ],
+      genres: [
+        "роман"
+      ],
+      language: "английский",
+      nationality: "угандийка",
+      awards: [
+        "Премия Кейн"
+      ],
+      tags: [
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "moses_isegawa",
+      name: "Мозес Исекава",
+      fullName: "Moses Isegawa",
+      years: "1963–",
+      birthDate: "1963",
+      deathDate: "",
+      birthPlace: "Уганда",
+      portrait: "",
+      bio: "Угандийско-нидерландский писатель, известный романами о судьбе Уганды.",
+      works: [
+        "Абиссинские хроники"
+      ],
+      genres: [
+        "роман"
+      ],
+      language: "английский",
+      nationality: "угандиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "robert_serumaga",
+      name: "Роберт Серумага",
+      fullName: "Robert Serumaga",
+      years: "1939–1980",
+      birthDate: "1939",
+      deathDate: "1980",
+      birthPlace: "Уганда",
+      portrait: "",
+      bio: "Угандийский драматург и писатель, один из основателей современной театральной традиции страны.",
+      works: [
+        "Majangwa"
+      ],
+      genres: [
+        "драма"
+      ],
+      language: "английский",
+      nationality: "угандиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "byron_kawadwa",
+      name: "Байрон Каваду",
+      fullName: "Byron Kawadwa",
+      years: "1940–1977",
+      birthDate: "1940",
+      deathDate: "1977",
+      birthPlace: "Уганда",
+      portrait: "",
+      bio: "Угандийский драматург и театральный деятель.",
+      works: [],
+      genres: [
+        "драма"
+      ],
+      language: "английский",
+      nationality: "угандиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
