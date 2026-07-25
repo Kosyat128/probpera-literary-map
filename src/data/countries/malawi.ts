041bee1516ad11fb87a024165cd1b73b9f727ee8
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:01:52 +0300

    Create malawi.ts

---
+++ src/data/countries/malawi.ts
@@ -0,0 +1,139 @@
+import type { Country } from "../types";
+
+export const malawi: Country = {
+  id: "malawi",
+  name: "Малави",
+  code: "mw",
+
+  writers: [
+    {
+      id: "legson_kayira",
+      name: "Легсон Кайира",
+      fullName: "Legson Kayira",
+      years: "1942–2012",
+      birthDate: "1942-05-10",
+      deathDate: "2012-10-14",
+      birthPlace: "Малави",
+      portrait: "",
+      bio: "Малавийский писатель, один из наиболее известных авторов страны.",
+      works: [
+        "I Will Try",
+        "The Detainee"
+      ],
+      genres: [
+        "роман",
+        "автобиографическая проза"
+      ],
+      language: "английский",
+      nationality: "малавиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "jack_mapanje",
+      name: "Джек Мапанье",
+      fullName: "Jack Mapanje",
+      years: "1944–",
+      birthDate: "1944-03-25",
+      deathDate: "",
+      birthPlace: "Малави",
+      portrait: "",
+      bio: "Малавийский поэт и один из крупнейших представителей африканской англоязычной поэзии.",
+      works: [
+        "Of Chameleons and Gods",
+        "The Last of the Sweet Bananas"
+      ],
+      genres: [
+        "поэзия"
+      ],
+      language: "английский",
+      nationality: "малавиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "steve_chimombo",
+      name: "Стив Чимомбо",
+      fullName: "Steve Chimombo",
+      years: "1945–2015",
+      birthDate: "1945",
+      deathDate: "2015",
+      birthPlace: "Малави",
+      portrait: "",
+      bio: "Малавийский писатель, поэт и драматург.",
+      works: [],
+      genres: [
+        "поэзия",
+        "драма"
+      ],
+      language: "английский",
+      nationality: "малавиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "frank_chipasula",
+      name: "Фрэнк Чипасула",
+      fullName: "Frank Chipasula",
+      years: "1949–",
+      birthDate: "1949",
+      deathDate: "",
+      birthPlace: "Малави",
+      portrait: "",
+      bio: "Малавийский поэт и литературный редактор.",
+      works: [
+        "Visions and Reflections"
+      ],
+      genres: [
+        "поэзия"
+      ],
+      language: "английский",
+      nationality: "малавиец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "paul_tiyambe_zeleza",
+      name: "Пол Тиямбе Зелеза",
+      fullName: "Paul Tiyambe Zeleza",
+      years: "1955–",
+      birthDate: "1955",
+      deathDate: "",
+      birthPlace: "Малави",
+      portrait: "",
+      bio: "Малавийский писатель, историк и автор эссеистики.",
+      works: [
+        "The Joys of Exile"
+      ],
+      genres: [
+        "эссе",
+        "проза"
+      ],
+      language: "английский",
+      nationality: "малавиец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
