851e9c86a2e9cf7aed2d15f82e2b1cc8e1d0f0e2
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:37:29 +0300

    Create equatorial_guinea.ts

---
+++ src/data/countries/equatorial_guinea.ts
@@ -0,0 +1,92 @@
+import type { Country } from "../types";
+
+export const equatorialGuinea: Country = {
+  id: "equatorial_guinea",
+  name: "Экваториальная Гвинея",
+  code: "gq",
+
+  writers: [
+    {
+      id: "donato_ndongo_bidyogo",
+      name: "Донато Ндонго-Бидиого",
+      fullName: "Donato Ndongo-Bidyogo",
+      years: "1950–",
+      birthDate: "1950-12-12",
+      deathDate: "",
+      birthPlace: "Бата, Экваториальная Гвинея",
+      portrait: "",
+      bio: "Экватогвинейский писатель, журналист и литературный исследователь. Один из основателей современной испаноязычной литературы Экваториальной Гвинеи.",
+      works: [
+        "Las tinieblas de tu memoria negra",
+        "Los poderes de la tempestad",
+        "El metro"
+      ],
+      genres: [
+        "роман",
+        "эссе",
+        "проза"
+      ],
+      language: "испанский",
+      nationality: "экватогвинеец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "maria_nsue_angue",
+      name: "Мария Нсуэ Ангюэ",
+      fullName: "María Nsué Angüe",
+      years: "1945–2017",
+      birthDate: "1945",
+      deathDate: "2017-01-18",
+      birthPlace: "Бата, Экваториальная Гвинея",
+      portrait: "",
+      bio: "Экватогвинейская писательница, одна из первых известных женщин-романисток страны.",
+      works: [
+        "Ekomo"
+      ],
+      genres: [
+        "роман"
+      ],
+      language: "испанский",
+      nationality: "экватогвинейка",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "juan_tomas_avila_laurel",
+      name: "Хуан Томас Авила Лаурель",
+      fullName: "Juan Tomás Ávila Laurel",
+      years: "1966–",
+      birthDate: "1966",
+      deathDate: "",
+      birthPlace: "Малабо, Экваториальная Гвинея",
+      portrait: "",
+      bio: "Современный экватогвинейский писатель, автор романов, рассказов и эссе. Один из наиболее известных представителей литературы страны.",
+      works: [
+        "Avión de ricos, ladrón de cerdos",
+        "Arde el monte de noche"
+      ],
+      genres: [
+        "роман",
+        "рассказ",
+        "эссе"
+      ],
+      language: "испанский",
+      nationality: "экватогвинеец",
+      awards: [],
+      tags: [
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
