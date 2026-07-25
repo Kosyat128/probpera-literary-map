99d50ade6b2610e4ab07b3176e4acf82c8d6aca0
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:43:22 +0300

    Add Luxembourg country data with writers

---
+++ src/data/countries/luxembourg.ts
@@ -0,0 +1,136 @@
+import type { Country } from "../types";
+
+export const luxembourg: Country = {
+  id: "luxembourg",
+  name: "Люксембург",
+  code: "lu",
+
+  writers: [
+    {
+      id: "michel_rodange",
+      name: "Мишель Роданж",
+      fullName: "Michel Rodange",
+      years: "1827–1876",
+      birthDate: "1827-01-03",
+      deathDate: "1876-08-27",
+      birthPlace: "Вальдбилиг, Люксембург",
+      portrait: "",
+      bio: "Классик люксембургской литературы, автор национального эпоса.",
+      works: [
+        "Renert oder de Fuuss am Frack an a Maansgréisst"
+      ],
+      genres: [
+        "поэма"
+      ],
+      language: "люксембургский",
+      nationality: "люксембуржец",
+      awards: [],
+      tags: [
+        "XIX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "edmond_de_la_fontaine",
+      name: "Эдмон де ла Фонтен",
+      fullName: "Edmond de la Fontaine",
+      years: "1823–1891",
+      birthDate: "1823-07-24",
+      deathDate: "1891-06-24",
+      birthPlace: "Люксембург",
+      portrait: "",
+      bio: "Люксембургский поэт и драматург, известный под псевдонимом Dicks.",
+      works: [
+        "De Scholtscheîn",
+        "D'Mumm Séis"
+      ],
+      genres: [
+        "драма",
+        "поэзия"
+      ],
+      language: "люксембургский",
+      nationality: "люксембуржец",
+      awards: [],
+      tags: [
+        "XIX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "anise_koltz",
+      name: "Аниз Кольц",
+      fullName: "Anise Koltz",
+      years: "1928–2023",
+      birthDate: "1928-06-12",
+      deathDate: "2023-03-01",
+      birthPlace: "Люксембург",
+      portrait: "",
+      bio: "Одна из крупнейших люксембургских поэтесс.",
+      works: [],
+      genres: [
+        "поэзия"
+      ],
+      language: "французский",
+      nationality: "люксембурженка",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "jean_portante",
+      name: "Жан Портанте",
+      fullName: "Jean Portante",
+      years: "1950–",
+      birthDate: "1950-12-19",
+      deathDate: "",
+      birthPlace: "Differdange, Люксембург",
+      portrait: "",
+      bio: "Люксембургский писатель, поэт и переводчик.",
+      works: [],
+      genres: [
+        "роман",
+        "поэзия"
+      ],
+      language: "французский",
+      nationality: "люксембуржец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "guy_helminger",
+      name: "Ги Хеллингер",
+      fullName: "Guy Helminger",
+      years: "1963–",
+      birthDate: "1963",
+      deathDate: "",
+      birthPlace: "Эш-сюр-Альзетт, Люксембург",
+      portrait: "",
+      bio: "Современный люксембургский писатель и драматург.",
+      works: [],
+      genres: [
+        "роман",
+        "рассказ",
+        "театр"
+      ],
+      language: "немецкий",
+      nationality: "люксембуржец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
