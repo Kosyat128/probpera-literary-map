c12f5dae8854c25d8b234c322b9ac3f7b1cd5f02
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:30:36 +0300

    Create mozambique1.ts

---
+++ src/data/countries/mozambique1.ts
@@ -0,0 +1,176 @@
+import type { Country } from "../types";
+
+export const mozambique: Country = {
+  id: "mozambique",
+  name: "Мозамбик",
+  code: "mz",
+
+  writers: [
+    {
+      id: "jose_craveirinha",
+      name: "Жозе Кравейринья",
+      fullName: "José Craveirinha",
+      years: "1922–2003",
+      birthDate: "1922-05-28",
+      deathDate: "2003-02-06",
+      birthPlace: "Мапуту, Мозамбик",
+      portrait: "",
+      bio: "Мозамбикский поэт, журналист и один из крупнейших классиков литературы страны. Первый африканский автор, получивший Премию Камоэнса.",
+      works: [
+        "Xigubo",
+        "Karingana ua Karingana"
+      ],
+      genres: [
+        "поэзия"
+      ],
+      language: "португальский",
+      nationality: "мозамбикец",
+      awards: [
+        "Премия Камоэнса (1991)"
+      ],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "orlando_mendes",
+      name: "Орланду Мендеш",
+      fullName: "Orlando Mendes",
+      years: "1916–1990",
+      birthDate: "1916-08-04",
+      deathDate: "1990-01-11",
+      birthPlace: "Илья-де-Мосамбик, Мозамбик",
+      portrait: "",
+      bio: "Один из первых крупных романистов Мозамбика, представитель ранней португалоязычной литературы страны.",
+      works: [
+        "Portagem"
+      ],
+      genres: [
+        "роман",
+        "проза"
+      ],
+      language: "португальский",
+      nationality: "мозамбикец",
+      awards: [],
+      tags: [
+        "XX век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "mia_couto",
+      name: "Миа Коуту",
+      fullName: "Mia Couto",
+      years: "1955–",
+      birthDate: "1955-07-05",
+      deathDate: "",
+      birthPlace: "Бейра, Мозамбик",
+      portrait: "",
+      bio: "Мозамбикский писатель, один из наиболее известных современных авторов Африки.",
+      works: [
+        "Terra Sonâmbula",
+        "O Último Voo do Flamingo"
+      ],
+      genres: [
+        "роман",
+        "проза"
+      ],
+      language: "португальский",
+      nationality: "мозамбикец",
+      awards: [
+        "Премия Камоэнса (2013)"
+      ],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "paulina_chiziane",
+      name: "Паулина Чизьяне",
+      fullName: "Paulina Chiziane",
+      years: "1955–",
+      birthDate: "1955-06-04",
+      deathDate: "",
+      birthPlace: "Манджакозе, Мозамбик",
+      portrait: "",
+      bio: "Мозамбикская писательница, первая женщина-романист страны.",
+      works: [
+        "Balada de Amor ao Vento",
+        "Niketche"
+      ],
+      genres: [
+        "роман"
+      ],
+      language: "португальский",
+      nationality: "мозамбичка",
+      awards: [
+        "Премия Камоэнса (2021)"
+      ],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "lilia_momple",
+      name: "Лилия Момпле",
+      fullName: "Lília Momplé",
+      years: "1935–",
+      birthDate: "1935-03-19",
+      deathDate: "",
+      birthPlace: "Илья-де-Мосамбик, Мозамбик",
+      portrait: "",
+      bio: "Мозамбикская писательница, известная социальной прозой и произведениями о колониальном прошлом страны.",
+      works: [
+        "Ninguém Matou Suhura",
+        "Neighbours"
+      ],
+      genres: [
+        "проза",
+        "роман"
+      ],
+      language: "португальский",
+      nationality: "мозамбичка",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    },
+
+    {
+      id: "eduardo_white",
+      name: "Эдуарду Уайт",
+      fullName: "Eduardo White",
+      years: "1963–2014",
+      birthDate: "1963-11-21",
+      deathDate: "2014-08-24",
+      birthPlace: "Келимане, Мозамбик",
+      portrait: "",
+      bio: "Мозамбикский поэт и писатель, один из заметных представителей современной литературы страны.",
+      works: [
+        "O País de Mim"
+      ],
+      genres: [
+        "поэзия"
+      ],
+      language: "португальский",
+      nationality: "мозамбикец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
