a55fbfd78958d7b1175eed666f2e0d56fde27110
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:47:50 +0300

    Create san_marino.ts

---
+++ src/data/countries/san_marino.ts
@@ -0,0 +1,33 @@
+import type { Country } from "../types";
+
+export const sanMarino: Country = {
+  id: "san_marino",
+  name: "Сан-Марино",
+  code: "sm",
+
+  writers: [
+    {
+      id: "marino_fattori",
+      name: "Марино Фаттори",
+      fullName: "Marino Fattori",
+      years: "1832–1896",
+      birthDate: "1832-03-25",
+      deathDate: "1896-04-27",
+      birthPlace: "Сан-Марино",
+      portrait: "",
+      bio: "Саммаринский поэт и писатель, один из представителей национальной литературной традиции Сан-Марино XIX века.",
+      works: [],
+      genres: [
+        "поэзия",
+        "проза"
+      ],
+      language: "итальянский",
+      nationality: "саммаринец",
+      awards: [],
+      tags: [
+        "XIX век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
