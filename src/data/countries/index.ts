b88f15f1b2529bb63c864e4a6ba8a1e73fc11dac
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 02:05:12 +0300

    Create countries database entry point

---
+++ src/data/countries/index.ts
@@ -0,0 +1,9 @@
+import type { Country } from "./types";
+
+/**
+ * Central entry point for the LiteraryMap countries database.
+ *
+ * Country modules are stored separately in this folder.
+ * They will be connected here as the main countries registry.
+ */
+export const countries: Country[] = [];
