0b8bc1d7724a6cc36a776735311f10dc9d214223
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 01:44:59 +0300

    Create nauru.ts

---
+++ src/data/countries/nauru.ts
@@ -0,0 +1,34 @@
+import type { Country } from "../types";
+
+export const nauru: Country = {
+  id: "nauru",
+  name: "Науру",
+  code: "nr",
+
+  writers: [
+    {
+      id: "michael_francis",
+      name: "Майкл Фрэнсис",
+      fullName: "Michael Francis",
+      years: "1960–",
+      birthDate: "1960",
+      deathDate: "",
+      birthPlace: "Науру",
+      portrait: "",
+      bio: "Науруанский автор, связанный с современной литературной и культурной традицией страны.",
+      works: [],
+      genres: [
+        "проза",
+        "эссе"
+      ],
+      language: "английский",
+      nationality: "науруанец",
+      awards: [],
+      tags: [
+        "XX век",
+        "XXI век"
+      ],
+      articleUrl: ""
+    }
+  ]
+};
