8ed14f9fd05b89a6d553fa902348b433a9c72375
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 06:23:57 +0300

    Add helper to resolve related writer ids into names

---
+++ src/utils/resolveRelatedWriters.ts
@@ -0,0 +1,10 @@
+import { countries } from "../data/countries";
+
+export function resolveRelatedWriters(ids: string[] = []) {
+  const writers = countries.flatMap((country) => country.writers || []);
+
+  return ids
+    .map((id) => writers.find((writer) => writer.id === id))
+    .filter(Boolean)
+    .map((writer) => writer?.fullName || writer?.name || "");
+}
