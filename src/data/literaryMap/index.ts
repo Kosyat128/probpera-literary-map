45fe3f82d38dbfd947b6313cda2728ae365fc8d8
Kosyat128 <Kosyat128@gmail.com>
2026-07-26 03:22:24 +0300

    Fix literary map type export after database migration

--- src/data/literaryMap/index.ts
+++ src/data/literaryMap/index.ts
@@ -1,2 +1,4 @@
 export { literaryCountries } from "./countries";
-export type { LiteraryCountry } from "./countries";
+
+export type LiteraryCountry =
+  (typeof import("./countries").literaryCountries)[keyof typeof import("./countries").literaryCountries];
