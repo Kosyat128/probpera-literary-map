248988fef480201f1f6330d501c5294d60115f9d
Kosyat128 <Kosyat128@gmail.com>
2026-08-23 09:23:17 +0300

    feat(i18n): serve localized country archive to the app

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,6 +1,15 @@
-export {
-  bookArchiveCountries,
-  countries,
+import {
+  bookArchiveCountries as sourceBookArchiveCountries,
+  countries as sourceCountries,
   generatedWriterDraftCount,
 } from "./countries/index";
+import { countryWithActiveLanguage } from "./countryLocalization";
+
+export const countries = sourceCountries.map((country) =>
+  countryWithActiveLanguage(country)
+);
+export const bookArchiveCountries = sourceBookArchiveCountries.map((country) =>
+  countryWithActiveLanguage(country)
+);
+export { generatedWriterDraftCount };
 export type { Country, WriterProfile as Writer } from "./countries/types";
